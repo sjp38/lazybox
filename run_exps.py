3488c021aafb69dd3e4b1eea5ca3269f411c56bc
SeongJae Park <sj38.park@gmail.com>
2013-11-05 21:58:15 +0900

    Print usage when user executed in wrong way
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- run_exps.py
+++ run_exps.py
@@ -50,6 +50,10 @@ class Exp:
         return self.__str__()
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "USAGE: %s <path to experiments spec file>" % sys.argv[0]
+        exit(1)
+
     exps = []
     starts = []
     mains = []
