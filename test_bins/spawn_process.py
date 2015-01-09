e42c60a2895b6e81b080e3bd5c402f0744ac4739
SeongJae Park <sj38.park@gmail.com>
2015-01-09 17:25:08 +0900

    test: add recursive child process termination problem
    If background process spawn child recursively, pkill can't kill whole
    child processes. ./run_exps.py
    test_bins/childs_processes_background.exps will create many child
    processes of spawn_process.py but they will not be terminated after main
    process(run_secs.py 6) be terminated. By TDD principle, it means test
    fails.
    
    Also, reconstruct directory. Move test related executables and configs
    into seperated directory.

---
+++ test_bins/spawn_process.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+import time
+
+remaining_spawns = int(sys.argv[1]) - 1
+
+if remaining_spawns > 0:
+    print 'will spawn %d more child' % remaining_spawns
+    cmd = '%s %d' % (__file__, remaining_spawns)
+    subprocess.call(cmd, shell=True)
+
+while True:
+    time.sleep(3)
