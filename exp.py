40f58ff358d759ae1ea9540bd6ee5bc083bfb55c
SeongJae Park <sj38.park@gmail.com>
2015-02-24 17:23:20 +0900

    exp: run concurrent main workloads like infinite background workload
    If multiple main workloads specified, make main workloads terminated
    earlier than others run as infinite background workload. Those tasks
    keeps running until slowest main workload be terminated.
    
    In other words, every multiple main workloads run concurrently until
    whole workloads be terminated at least once.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- exp.py
+++ exp.py
@@ -10,6 +10,16 @@ __license__ = "GPLv3"
 import os
 import signal
 import subprocess
+import time
+
+class Task:
+    cmd = None
+    popn = None
+    completed = False
+
+    def __init__(self, cmd, popn):
+        self.cmd = cmd
+        self.popn = popn
 
 class Exp:
     start_cmds = []
@@ -17,7 +27,7 @@ class Exp:
     main_cmds = []
     back_cmds = []
 
-    main_procs = []
+    main_tasks = []
     back_procs = []
 
     def __init__(self, start, main, back, end):
@@ -35,18 +45,38 @@ class Exp:
 
     def execute(self):
         self.back_procs = []
+        self.main_tasks = []
         print "do exp %s" % self
         for start in self.start_cmds:
             subprocess.call(start, shell=True, executable="/bin/bash")
         for back in self.back_cmds:
             self.back_procs.append(subprocess.Popen(back, shell=True,
                 executable="/bin/bash"))
+
         for main in self.main_cmds:
-            self.main_procs.append(subprocess.Popen(main, shell=True,
-                executable="/bin/bash"))
-        for main_proc in self.main_procs:
-            main_proc.wait()
-        print "workload done. kill back procs."
+            self.main_tasks.append(Task(main, subprocess.Popen(main,
+                shell=True, executable="/bin/bash")))
+
+        # If more than one main tasks specified, tasks terminated earlier
+        # become infinite background job until slowest main task be terminated.
+        nr_completed = 0
+        while nr_completed < len(self.main_tasks):
+            time.sleep(0.5)
+            for task in self.main_tasks:
+                if task.popn.poll() == None:
+                    continue
+                print "%s(%s) terminated" % (task.cmd, task.popn.pid)
+                task.completed = True
+                nr_completed = sum(t.completed for t in self.main_tasks)
+                if nr_completed < len(self.main_tasks):
+                    task.popn = subprocess.Popen(task.cmd, shell=True,
+                            executable="/bin/bash")
+        print "whole main workloads done; kill zombie main procs"
+        for task in self.main_tasks:
+            if task.popn.poll() == None:
+                kill_childs_self(task.popn.pid)
+
+        print "kill background procs"
         for back_proc in self.back_procs:
             childs = all_childs(back_proc.pid)
             for child in reversed(childs):
@@ -63,6 +93,24 @@ class Exp:
             os.kill(back_proc.pid, signal.SIGTERM)
         for end in self.end_cmds:
             subprocess.call(end, shell=True, executable="/bin/bash")
+
+def kill_childs_self(pid):
+    childs = all_childs(pid)
+    for child in reversed(childs):
+        if child == pid:
+            continue
+        try:
+            print "kill child: ", child
+            os.kill(child, signal.SIGTERM)
+        except OSError as e:
+            print "error %s occurred while killing child %s" % (e, child)
+    print "kill processes with ppid: ", pid
+    subprocess.call('pkill -P %d' % pid, shell=True)
+    try:
+        print "kill self: %s" % pid
+        os.kill(pid, signal.SIGTERM)
+    except OSError as e:
+        print "error %s occurred while killing self %s" % (e, pid)
 
 def all_childs(pid):
     childs = []
