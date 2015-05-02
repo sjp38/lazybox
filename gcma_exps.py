3711877ec2ce9f3d5d046e2227f14e17a7692dd1
SeongJae Park <sj38.park@gmail.com>
2015-05-02 19:57:38 +0900

    add automated script for gcma
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ gcma_exps.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+USAGE="%s <user name> <target> <ssh port> <password>"
+
+if len(sys.argv) < 5:
+    print "usage: ", USAGE
+    print ""
+    exit(1)
+
+user = sys.argv[1]
+target = sys.argv[2]
+port = sys.argv[3]
+password = sys.argv[4]
+
+lbpath = "/home/%s/lazybox" % user
+
+bootloader = "grub"
+if target == "raspberrypi":
+    bootloader = "rasp2"
+
+k_cma = "cma"
+k_gcma = "gcma"
+k_vanilla = "vanilla"
+kernels = [k_cma, k_gcma, k_vanilla]
+kparam_cma = "coherent_pool=16M cma=64M smsc95xx.turbo_mode=N"
+expspath = "./exps/"
+exps = ["gcma", "gcma-blogbench",
+        "still", "still-blogbench", "blogbench-still", "blogbench"]
+
+for kernel in kernels:
+    for exp in exps:
+        if kernel in [k_cma, k_gcma]:
+            kernel_param = kparam_cma
+        if kernel == k_vanilla and (exp == "gcma" or exp == "gcma-blogbench"):
+            continue
+        os.system("expect ./remote_set_kernel.exp %s %s %s %s %s %s %s %s" % (
+            user, target, port, password, lbpath, bootloader, kernel, kernel_param))
+        os.system("expect ./remote_exps.exp %s %s %s %s %s %s" % (
+            user, target, port, password, lbpath, expspath + exp))
