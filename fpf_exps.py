ffd6c475e27f4bd90b48c3d7fba32f9a3f937fc6
SeongJae Park <sj38.park@gmail.com>
2015-11-25 16:20:39 +0900

    fpf: automated multiple configured workloads
    Now, fpf can be configured to do specific set of operations using
    argument. This commit automate fpf_exps.py to run multiple configured
    operations. By this change, it would be more easy to know cost of each
    operations and their variable configurations.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- fpf_exps.py
+++ fpf_exps.py
@@ -13,20 +13,27 @@ exps_paths = ["exps/pktgen_todcslab", "exps/pktgen_trafficsample",
                 "exps/pktgen_wireshark"]
 fpf_path = "%s/fpf/src/build/fpf" % homepath
 
+fpf_options = ["-p1 -H1 -c1 -m1", "-p1 -H0 -c0 -m0", "-p1 -H1 -c0 -m0",
+                "-p1 -H0 -c0 -m1", "-p1 -H0 -c1 -m0", "-p1 -H0 -c1 -m1",
+                "-p0 -H0 -c1 -m1", "-p0 -H0 -c1 -m0",
+                "-p0 -H0 -c0 -m0"]
+
 exp = ""
-for exps_path in exps_paths:
-    start_cmd = "start echo '==========================='\n"
-    start_cmd += "start echo '%s'\n" % exps_path
-    start_cmd += "start echo '==========================='"
-    main_cmd = "main sleep 30"
-    back_cmd = "back %s %s %s %s %s %s %s > /dev/null\n" % (
-            remote_exps_cmd,
-            user, target, port, password, lazybox_path, exps_path)
-    back_cmd += "back %s -c 1ff -n 4 -- -f %s/fpf/data/umd5_10m.fpf" % (
-            fpf_path, homepath)
-    exp = "%s\n%s\n%s\n\n" % (start_cmd, main_cmd, back_cmd)
-    with open("tmp.exp", 'w') as f:
-        f.write(exp)
-    os.system("./run_exps.py tmp.exp")
-    time.sleep(5)
+for option in fpf_options:
+    for exps_path in exps_paths:
+        start_cmd = "start echo '==========================='\n"
+        start_cmd += "start echo '%s'\n" % exps_path
+        start_cmd += "start echo '==========================='"
+        main_cmd = "main sleep 28"
+        back_cmd = "back %s %s %s %s %s %s %s > /dev/null\n" % (
+                remote_exps_cmd,
+                user, target, port, password, lazybox_path, exps_path)
+        back_cmd += "back %s -c 1ffff -n 4 -- -f %s/fpf/data/umd5_10m.fpf %s" % (
+                fpf_path, homepath, option)
+        exp = "%s\n%s\n%s\n\n" % (start_cmd, main_cmd, back_cmd)
+        with open("tmp.exp", 'w') as f:
+            f.write(exp)
+        os.system("echo %s >> log" % (option + " " + exps_path))
+        os.system("./run_exps.py tmp.exp")
+        time.sleep(5)
 #os.system("rm ./tmp.exp")
