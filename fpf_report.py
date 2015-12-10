553b1d86685b5555d00224f0350af333eb2ef1e3
SeongJae Park <sj38.park@gmail.com>
2015-12-10 23:14:53 +0900

    fpf: report: add performance effect of varying workloads
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- fpf_report.py
+++ fpf_report.py
@@ -5,14 +5,18 @@ import os
 LOGPATH = "log"
 
 for traffic in ["wireshark", "syn64"]:
-    for workload in ["-p1 -H1 -m1 -c1 -t1",
-                    "-p1 -H1 -m1 -c1 -t40",
-                    "-p0 -H0 -m0 -c0 -t0",
-                    "-p0 -H0 -m0 -c0 -t1",
-                    "-p1 -H1 -m0 -c0 -t1",
-                    "-p0 -H0 -m1 -c1 -t0",
-                    "-p0 -H0 -m1 -c0 -t0",
-                    "-p0 -H0 -m0 -c1 -t0"]:
+    for workload in ["-p1 -H1 -m1 ' -c1 ' -t1",
+                    "-p1 -H1 -m1 ' -c1 ' -t40",
+                    "-p0 -H0 -m0 ' -c0 ' -t0",
+                    "-p0 -H0 -m0 ' -c0 ' -t1",
+                    "-p1 -H1 -m0 ' -c0 ' -t1",
+                    "-p0 -H0 -m1 ' -c1 ' -t0",
+                    "-p0 -H0 -m1 ' -c0 ' -t0",
+                    "-p0 -H0 -m0 ' -c1 ' -t0",
+                    "' -c3 '",
+                    "' -c1f '",
+                    "' -c1ff '",
+                    "' -c1ffff '"]:
         keyword = traffic + " " + workload
         cmd = "./fpf_log_reduce.py %s %s" % (LOGPATH, keyword)
         os.system(cmd)
