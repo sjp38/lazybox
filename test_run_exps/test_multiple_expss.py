1400ba401a22c9ba9f591a4fdfcec718ba704649
SeongJae Park <sj38.park@gmail.com>
2016-01-08 11:30:51 +0900

    test_run_exps: add test for multiple exps spec files support
    Add a simple test for multiple test specification files. Though it do
    not automate the pass / fail check, it is worth to exist.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ test_run_exps/test_multiple_expss.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"test multiple experiments spec files support of run_exps.py"
+
+import os
+
+testdir = os.path.abspath(os.path.dirname(__file__))
+lzdir = os.path.join(os.path.dirname(__file__), os.pardir)
+lzdir = os.path.abspath(lzdir)
+cmd = "%s %s %s" % (os.path.join(lzdir, "run_exps.py"),
+        os.path.join(testdir, "hello"), os.path.join(testdir, "bye"))
+print "execute: ", cmd
+os.system(cmd)
