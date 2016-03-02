89bc008bc388ac49757e3a501c690f7a39d59c86
SeongJae Park <sj38.park@gmail.com>
2016-03-02 16:33:36 +0900

    add simple python wrapper for remote_exps.exp
    For some people, expect is not a familiar tool.  For the case, this
    commit adds a new python wrapper for `remote_exps.exp`.  The python
    wrapper works for one remote experiment.  It may be used inside
    experiment specification file as `main`, `back`, `end` or any type of
    task and be used as a input to familiar `run_exps.py`.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ remote_exp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import ssh_args
+
+USAGE="%s <user name> <target> <ssh port> [password] [exp]" % sys.argv[0]
+
+user, target, port, password = ssh_args.parse_input()
+if len(sys.argv) < 6:
+    exp = raw_input("exp to run in remote: ")
+else:
+    exp = sys.argv[5]
+
+lbpath = "/home/%s/lazybox" % user
+
+cmd = "expect ./remote_exps.exp %s %s %s %s %s %s" % (
+        user, target, port, password, lbpath, exp)
+print "[remote_exp.py] do cmd $ ", cmd
+os.system(cmd)
