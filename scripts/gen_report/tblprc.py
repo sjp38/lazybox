f5d35cb80e9cec47d8f4df15cdc49cc317a547a4
SeongJae Park <sj38.park@gmail.com>
2016-06-06 14:26:12 +0900

    tblprc: change table inner struct
    This commit changes table inner struct for row from list of values to
    dictionary.  The dictionary has column name as key and value as value.
    However, the constructor of table is still receiving list of list as
    rows as before.  It is intended to be easy for writing test code.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/gen_report/tblprc.py
+++ scripts/gen_report/tblprc.py
@@ -8,14 +8,15 @@ import math
 class ATable:
     title = None    # string
     legend = None   # list of strings
-    colidx = None
-    rows = None     # list of lists of strings
+    rows = None     # list of Maps[column name: value]
 
     def __init__(self, title, legend, rows):
+        """rows is list of list."""
         self.title = title
         self.legend = legend
-        self.rows = rows
-        self.set_colidx()
+        self.rows = []
+        for row in rows:
+            self.rows.append({legend[i]: row[i] for i in range(len(legend))})
 
     def __repr__(self):
         return self.__str__()
@@ -24,26 +25,21 @@ class ATable:
         str_ = "title: %s\n" % self.title
         str_ += "%s" % self.legend + "\n"
         for row in self.rows:
-            str_ += "%s" % row + "\n"
+            str_ += "%s\n" % [row[cname] for cname in self.legend]
         return str_
 
-    def set_colidx(self):
-        if self.colidx == None:
-            self.colidx = {}
-        for idx, name in enumerate(self.legend):
-            self.colidx[name] = idx
-
     def item_at(self, row, col):
-        return self.rows[row][self.colidx[col]]
+        return self.rows[row][col]
 
     def update_at(self, row, col, val):
-        self.rows[row][self.colidx[col]] = val
+        self.rows[row][col] = val
 
     def replace_legend(self, oldlegend, newlegend):
         for idx, name in enumerate(self.legend):
             if name == oldlegend:
                 self.legend[idx] = newlegend
-        self.set_colidx()
+        for row in self.rows:
+            row[newlegend] = row.pop(oldlegend)
         return self
 
     def get_title(self):
@@ -55,13 +51,12 @@ class ATable:
     def append_column(self, col_name, generator):
         self.legend.append(col_name)
         for idx in range(len(self.rows)):
-            self.rows[idx].append(generator(idx))
+            self.rows[idx][col_name] = generator(idx)
         return self
 
-    def convert_column(self, column, converter):
+    def convert_column(self, col_name, converter):
         for idx in range(len(self.rows)):
-            self.rows[idx][column] = converter(idx)
-        self.set_colidx()
+            self.rows[idx][col_name] = converter(idx)
         return self
 
     def csv(self):
@@ -69,7 +64,8 @@ class ATable:
         lines.append("title, %s" % self.title)
         lines.append(", ".join(str(x) for x in self.legend))
         for row in self.rows:
-            lines.append(", ".join(str(x) for x in row))
+            lines.append(", ".join(
+                [str(row[cname]) for cname in self.legend]))
         return '\n'.join(lines)
 
 def from_csv(csv):
@@ -82,20 +78,11 @@ def from_csv(csv):
         rows.append([x.strip() for x in line.split(',')])
     return ATable(title, legend, rows)
 
-def keyindexs(legend, keys):
-    kidxs = []
-    for k in keys:
-        for idx, name in enumerate(legend):
-            if k == name:
-                kidxs.append(idx)
-    return kidxs
-
 def pick_fields(table, fields):
     """Reconstruct table with selected fields only"""
-    fields_idxs = keyindexs(table.legend, fields)
     new_rows = []
     for row in table.rows:
-        new_rows.append([row[idx] for idx in fields_idxs])
+        new_rows.append([row[col] for col in fields])
     return ATable(table.title, fields, new_rows)
 
 def merge(tables):
@@ -103,6 +90,7 @@ def merge(tables):
 
     Tables should have same legend and same number of rows.  If tables have
     differenct number of rows, compensate() function may be helpful.
+    Each name of tables should be unique.
     """
     new_legend = []
     for table in tables:
@@ -114,7 +102,7 @@ def merge(tables):
         for ridx, row in enumerate(table.rows):
             if idx == 0:
                 new_rows.append([])
-            new_rows[ridx].extend(row)
+            new_rows[ridx].extend([row[col] for col in tables[0].legend])
     return ATable('-'.join([table.title for table in tables]),
             new_legend, new_rows)
 
@@ -127,22 +115,21 @@ def merge_vertical(tables):
         return new_rows
 
     for table in tables:
-        new_rows.extend(table.rows)
+        for row in table.rows:
+            new_rows.append([row[col] for col in tables[0].legend])
     return ATable('-'.join([table.title for table in tables]),
             tables[0].legend, new_rows)
 
-def split_with_key(tables, keys):
+def split_with_key(table, keys):
     """Split tables into multiple tables with same keys.
 
     Title of splitted tables will be the keys.
     """
-    kidxs = keyindexs(tables.legend, keys)
-
     inter_map = {}
-    for row in tables.rows:
-        key = "%s" % '-'.join([str(row[idx]) for idx in kidxs])
+    for row in table.rows:
+        key = '-'.join([str(row[key]) for key in keys])
         if not inter_map.has_key(key):
-            inter_map[key] = ATable(key, tables.legend, [])
+            inter_map[key] = ATable(key, table.legend, [])
         inter_map[key].rows.append(row)
     return inter_map.values()
 
@@ -159,9 +146,9 @@ def calc_stat(table, keys):
     with same keys.
     """
     new_legend = []
+    suffixes = ["_min", "_max", "_avg", "_stdev"]
     for name in table.legend:
-        new_legend.extend([name + "_min", name + "_max", name + "_avg",
-                            name + "_stdev"])
+        new_legend.extend([name + suffix for suffix in suffixes])
 
     new_rows = []
     ret = ATable(table.title, new_legend, [])
@@ -170,38 +157,37 @@ def calc_stat(table, keys):
     for subtable in tables:
         new_row = []
         new_rows.append(new_row)
-        for i in range(len(subtable.legend)):
+        for col in subtable.legend:
             try:
-                vals = [float(row[i]) for row in subtable.rows]
+                vals = [float(row[col]) for row in subtable.rows]
             except ValueError:
-                val = subtable.rows[0][i]
+                val = subtable.rows[0][col]
                 new_row.extend([val, val, val, val])
                 continue
             new_row.extend(__calc_stat(vals))
     return ATable(table.title, new_legend, new_rows)
 
 def sort_with(tables, keys):
-    kidxs = keyindexs(tables.legend, keys)
-    for i in reversed(kidxs):
-        tables.rows.sort(key=lambda x: x[i])
+    for key in reversed(keys):
+        tables.rows.sort(key=lambda x: x[key])
     return tables
 
-def compensate(tables, kidx, default_val):
+def compensate(tables, key_col, default_val):
     """Compensate tables to have same keys.
 
     Rows in tables should be sorted by the key."""
     total_keys = []
     for table in tables:
         for row in table.rows:
-            key = row[kidx]
+            key = row[key_col]
             if not key in total_keys:
                 total_keys.append(key)
     total_keys.sort()
     for table in tables:
         for idx, key in enumerate(total_keys):
-            if len(table.rows) <= idx or table.rows[idx][kidx] != key:
-                new_row = [default_val] * len(table.rows[0])
-                new_row[kidx] = key
+            if len(table.rows) <= idx or table.rows[idx][key_col] != key:
+                new_row = {col: default_val for col in table.legend}
+                new_row[key_col] = key
                 table.rows.insert(idx, new_row)
 
 if __name__ == "__main__":
@@ -210,10 +196,11 @@ if __name__ == "__main__":
                 [1, 1, 'a'], [1, 3, 'a'], [1, 5, 'a'],
                 [2, 3, 'b'], [2,4,'b'], [2,5,'b'], [3, 5, 'c']])
     stat_calced = calc_stat(t, ["key"])
-    sorted = sort_with(stat_calced, ["key_avg"])
-    print sorted
-    print sorted.csv()
-    print from_csv(sorted.csv())
+    sorted_ = sort_with(stat_calced, ["key_avg"])
+    print t
+    print sorted_
+    print sorted_.csv()
+    print from_csv(sorted_.csv())
     print sort_with(calc_stat(from_csv(t.csv()), ["key"]), ["key_avg"])
 
     t = ATable("foo", ["thrs", "system", "value1", "value2"], [
@@ -240,13 +227,13 @@ if __name__ == "__main__":
     t2 = ATable("bar", ["key", "val"], [[1, 3], [2, 4], [5, 7]])
     print t
     print t2
-    compensate([t, t2], 0, -1)
+    compensate([t, t2], "key", -1)
     print "compensated"
     print t
     print t2
 
     t = ATable("foo", ["key", "val"], [[1, 3], [3, 5]])
-    t.convert_column(0, lambda r: str(t.item_at(r, "val")))
+    t.convert_column("key", lambda r: str(t.item_at(r, "val")))
     print t
 
     t = ATable("foo", ["key", "val"], [[1, 3], [2, 4]])
