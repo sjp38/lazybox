4668525e8486856955deb327a3ab9d4331628923
SeongJae Park <sj38.park@gmail.com>
2017-05-25 15:30:24 +0900

    turn_thp: Use variable to reduce repeats
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/turn_thp.sh
+++ scripts/turn_thp.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+SYSFS_THP_ENABLED=/sys/kernel/mm/transparent_hugepage/enabled
+
 if [ $# -ne 1 ];
 then
 	echo "Usage: $0 <always|madvise|never>"
 	echo ""
 	echo "Current status: "
-	cat /sys/kernel/mm/transparent_hugepage/enabled
+	cat $SYSFS_THP_ENABLED
 	exit 1
 fi
 
-echo $1 > /sys/kernel/mm/transparent_hugepage/enabled
+echo $1 > $SYSFS_THP_ENABLED
