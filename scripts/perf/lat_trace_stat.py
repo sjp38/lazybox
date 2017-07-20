f31671265b3ad409bffea0423596906510e933c2
SeongJae Park <sj38.park@gmail.com>
2017-07-21 01:00:42 +0900

    lat_trace_stat: Check non sample case
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/perf/lat_trace_stat.py
+++ scripts/perf/lat_trace_stat.py
@@ -6,5 +6,11 @@ data = []
 for line in sys.stdin:
     data.append(int(line.split()[1]))
 
-print "avg, min, max"
-print "%.3f, %d, %d" % (sum(data) / float(len(data)), min(data), max(data))
+average = -1
+min_ = -1
+max_ = -1
+if len(data) > 0:
+    average = sum(data) / float(len(data))
+    min_ = min(data)
+    max_ = max(data)
+print "avg, min, max: %.3f, %d, %d" % (average, min_, max_)
