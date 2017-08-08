ff415f86a8f12e1bd88c8b83619ecfa6b5f4024a
SeongJae Park <sj38.park@gmail.com>
2017-08-09 02:22:12 +0900

    fetch-cloudsuite.sh: Check git installment
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- workloads/cloudsuite/fetch-cloudsuite.sh
+++ workloads/cloudsuite/fetch-cloudsuite.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if ! which git
+then
+	echo "[Error] git not found. Please install it."
+	exit 1
+fi
+
 # Because official cloudsuite is updated often, we use a fork.
 git clone https://github.com/sjp38/cloudsuite-personal.git cloudsuite
