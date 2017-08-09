256ec5ccd0aa23e9eec7c8d13fc09408cebb7f23
SeongJae Park <sj38.park@gmail.com>
2017-08-09 18:44:30 +0900

    repeat.sh: Allow -1 as inifnite count
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/repeat.sh
+++ scripts/repeat.sh
@@ -2,18 +2,17 @@
 
 # Execute a command specific times with given delay between execution
 
-USAGE="$0 <delay> <count> <command...>"
-
 if [ $# -lt 3 ]
 then
-	echo $USAGE
+	echo "$0 <delay> <count> <command...>"
+	echo "	Use -1 as count for infinite repeat."
 	exit 1
 fi
 
 DELAY=$1
 COUNT=$2
 
-for ((N = 0; N < $COUNT; N++))
+for ((N = 0; N < $COUNT || $COUNT == -1; N++))
 do
 	eval ${@:3}
 	sleep $DELAY
