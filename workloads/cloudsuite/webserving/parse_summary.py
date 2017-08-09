cabeeb396476f381d44d75321ac996efb95dde0e
SeongJae Park <sj38.park@gmail.com>
2017-08-10 02:39:33 +0900

    cloudsuite/webserving: Add script for summary parsing
    Summary of web-serving benchmark is still hard to read with the origin
    xml format.  This commit adds a simple script for parsing of the xml
    summary.  It parse the summary and prints out essential values such as
    number of users, ops/sec, and latencies.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ workloads/cloudsuite/webserving/parse_summary.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Parse summary.xml and print out requested field
+"""
+
+import xml.etree.ElementTree as ET
+import sys
+
+def walk_elem(elem, level=0):
+    outindent = "    " * level
+    indent = "    " * (level + 1)
+    print outindent, "{"
+    print indent, "tag: ", elem.tag
+    print indent, "attr: ", elem.attrib
+    print indent, "text: ", elem.text
+    for child in elem:
+        walk_elem(child, level + 1)
+    print outindent, "}"
+
+xmldat = "".join(sys.stdin)
+root = ET.fromstring(xmldat)
+#walk_elem(root, 0)
+
+print "ops:\t", root.find("./benchSummary/metric").text
+print "users:\t", root.find("./driverSummary/users").text
+
+response_times = root.findall("./driverSummary/responseTimes/operation")
+for op in response_times:
+    for lat in op:
+        if not lat.text:
+            continue
+        if lat.tag == "passed":
+            continue
+        if lat.tag == "percentile":
+            lat.tag = "%s%s %s" % (lat.attrib["nth"], lat.attrib["suffix"], lat.tag)
+        print "%s %s: %s" % (op.attrib["name"], lat.tag, lat.text)
