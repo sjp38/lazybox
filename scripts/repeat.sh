44ed0e523199d53669651a201f33bbfa245bd699
SeongJae Park <sj38.park@gmail.com>
2017-08-09 18:46:57 +0900

    repeat.sh: Use count -1 as infinite number
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/repeat.sh
+++ scripts/repeat.sh
@@ -4,7 +4,7 @@
 
 if [ $# -lt 3 ]
 then
-	echo "$0 <delay> <count> <command...>"
+	echo "USAGE: $0 <delay> <count> <command...>"
 	echo "	Use -1 as count for infinite repeat."
 	exit 1
 fi
@@ -12,7 +12,7 @@ fi
 DELAY=$1
 COUNT=$2
 
-for ((N = 0; N < $COUNT || $COUNT == -1; N++))
+for ((N = 0; $COUNT == -1 || N < $COUNT; N++))
 do
 	eval ${@:3}
 	sleep $DELAY
