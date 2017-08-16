aa9dee456ad791e40ddec11bf8eabdaa8c28840d
SeongJae Park <sj38.park@gmail.com>
2017-08-16 18:24:34 +0900

    gnuplot: Add script for line plotting of stdin data
    This commit adds a script for line plotting of stdin data.  It receives
    data from stdin and generate plot.pdf using the data in scattered chart
    type.  Using that, you can generate pdf chart from stdin without
    intermediate file generation effort.  The script also support calling
    path independence.  It can be called from anywhere unlike 'example.sh',
    which fails to run if it called from other directory rather than the
    self directory due to gnuplot load path problem.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ gnuplot/plot_stdin.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname $0`
+
+export GNUPLOT_LIB=$SCRIPT_DIR
+
+NR_TRY=0
+
+for ((i = 0; i < 5; i++))
+do
+	TMPFILE=/tmp/$i.gpdat
+	if [ ! -f $TMPFILE ]; then
+		break
+	fi
+	if [ $i -eq 4 ]; then
+		echo "/tmp/0-4.gpdat already exists.  Remove them first."
+		exit 1
+	fi
+done
+
+cat /dev/stdin > $TMPFILE
+
+function nr_records {
+	awk 'BEGIN{RS="\n\n\n";}{print NR;}' $1 | wc -l
+}
+
+gnuplot -e "DATA='$TMPFILE'; NR_IDXS='`nr_records $TMPFILE`'" ./example.line.gp
+
+rm $TMPFILE
