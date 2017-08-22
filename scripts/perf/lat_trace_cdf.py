3decf71e6662421b65ddc091448dedabe9012996
SeongJae Park <sj38.park@gmail.com>
2017-08-22 15:26:44 +0900

    lat_trace_cdf.py: Modify output format
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/perf/lat_trace_cdf.py
+++ scripts/perf/lat_trace_cdf.py
@@ -14,6 +14,6 @@ data = sorted(data)
 
 for i in range(precision):
     idx = len(data) / precision * i
-    print "%d, %d" % (100 / precision * i, data[idx])
+    print "%d %d" % (100 / precision * i, data[idx])
 
-print "%d, %d" % (100, data[-1])
+print "%d %d" % (100, data[-1])
