bb21309b2c52155bc86d8edc8a909b3ef1c34615
SeongJae Park <sj38.park@gmail.com>
2017-08-22 16:28:50 +0900

    scripts/perf: Add field picker for script output
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ scripts/perf/pick_field.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+
+if len(sys.argv) < 2:
+    print "Usage: %s <field name>" % sys.argv[0]
+    exit(1)
+
+# example input line is output of `$ perf script`.  It may looks as below.
+# command, pid, tid, timestamp, tracepoint name, and trace
+#
+# memwalk  3837 [012]   383.632199: kmem:mm_page_alloc: \
+#           page=0x2f95b76 pfn=49896310 order=0 migratetype=0 \
+#           gfp_flags=GFP_NOWAIT|__GFP_NOWARN
+
+wanted = sys.argv[1]
+
+for line in sys.stdin:
+    tokens = line.split()
+    time = float(tokens[3][:-1]) * 1000 * 1000 * 1000   # nano second level
+    for fields in tokens[5:]:
+        key, value = fields.split('=')
+        if key == wanted:
+            print "%d %s" % (int(time), value)
