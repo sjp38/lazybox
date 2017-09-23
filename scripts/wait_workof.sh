52c884f468fbd6b5457f1b41b05e9636882b8353
sjpark <sjpark@hydra.hydra>
2017-09-23 15:11:18 +0900

    Add script for work waiting
    Some tasks work in background.  Daemon processes such as mysqld can be
    an example.  To wait the background job, monitoring its status is
    necessary.  This commit adds a script for automation of the waiting.  It
    monitor status of processes of a command and wait until all of the
    processes becomes non-running status.
    
    Signed-off-by: sjpark <sjpark@hydra.hydra>

---
+++ scripts/wait_workof.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <process name>"
+	exit 1
+fi
+
+function nr_running() {
+	CMD=$1
+	pids=`pidof $CMD`
+	if [ pids == "" ]
+	then
+		echo "0"
+	fi
+
+	NR_RUNNING=0
+	for pid in $pids
+	do
+		if [ `grep "running" /proc/$pid/status | wc -l` -eq 1 ]
+		then
+			NR_RUNNING=$(( $NR_RUNNING + 1 ))
+		fi
+	done
+	echo $NR_RUNNING
+}
+
+CMD=$1
+while true;
+do
+	sleep 1
+	if [ `nr_running $CMD` == "0" ]
+	then
+		break
+	fi
+done
