5e0297f080ccc91a0d36fa5b0708d5e034ab9f8d
SeongJae Park <sj38.park@gmail.com>
2017-11-22 11:56:21 +0900

    memfp: Add argument check
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/memfp.sh
+++ scripts/memfp.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Print out memory footprint of a command in 1 second interval.
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <command name>"
+	exit 1
+fi
+
 COMM=$1
 
 echo "vsz	rss	comm"
