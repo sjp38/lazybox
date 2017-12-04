afd8b867c8cf583a9bff2e882b61227694607748
SeongJae Park <sj38.park@gmail.com>
2017-12-04 18:35:05 +0900

    plot_swptrace: Rename function
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/perf/plot_swptrace.sh
+++ scripts/perf/plot_swptrace.sh
@@ -2,7 +2,7 @@
 
 BINDIR=`dirname $0`
 
-foo() {
+parse_perfout() {
 	echo "Swpin"
 	perf script | grep swpin | sed -e 's/://' | sed -e 's/page=//' |
 		awk '{print $4 " " $7}'
@@ -13,6 +13,6 @@ foo() {
 		awk '{print $4 " " $7}'
 }
 
-foo |
+parse_perfout |
 	$BINDIR/../../gnuplot/plot_stdin.sh scatter \
 		"Time (seconds)" "Page Frame"
