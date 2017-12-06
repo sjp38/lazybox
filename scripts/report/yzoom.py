4988a45700aff8855b4e685bc76ba9e9a493d7c4
SeongJae Park <sj38.park@gmail.com>
2017-12-06 17:32:21 +0900

    yzoom: Clean code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/report/yzoom.py
+++ scripts/report/yzoom.py
@@ -10,7 +10,6 @@ ymin = int(sys.argv[1])
 ymax = int(sys.argv[2])
 
 for l in sys.stdin:
-	yval = int(l.split()[1], 0)
-	if yval > ymin and yval < ymax:
-		print l.strip()
-
+    yval = int(l.split()[1], 0)
+        if yval > ymin and yval < ymax:
+            print l.strip()
