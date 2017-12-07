20f53298d962341c139de95759b55246a87918b1
SeongJae Park <sj38.park@gmail.com>
2017-12-07 11:09:05 +0900

    statof: Add signle file stat extraction feature
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/report/statof.py
+++ scripts/report/statof.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 program_decr = """
-Make user specified status (average, min, max, stdev) from multiple files with
-same format.  The input file should be generated using `files_to.py`
+Make user specified status (average, min, max, stdev) from single or multiple
+files with same format.  In case of single file, it assumes the file to be in
+records and get statistic of second field only.  In case of multiple files, it
+assumes the files to be in table format.  The input file should be generated
+using `files_to.py`.
 """
 
 import argparse
@@ -24,9 +27,30 @@ args = parser.parse_args()
 
 target = vars(args)['stat']
 paths = vars(args)['files']
+
+def single_file_stat(path):
+    nrs = []
+    with open(path, 'r') as f:
+        for l in f:
+            nrs.append(float(int(l.split()[1], 0)))
+    if target == 'avg':
+        print sum(nrs) / len(nrs)
+    elif target == 'min':
+        print min(nrs)
+    elif target == 'max':
+        print max(nrs)
+    elif target == 'stdev':
+        avg = sum(nrs) / len(nrs)
+        variance = sum([pow(v - avg, 2) for v in nrs]) / len(nrs)
+        print math.sqrt(variance)
+
 if len(paths) == 0:
     parser.print_help()
     exit(1)
+
+if len(paths) == 1:
+    single_file_stat(paths[0])
+    exit(0)
 
 datas = []
 for p in paths:
