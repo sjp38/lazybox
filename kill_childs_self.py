ddb17ba911a6a5a06e77172dfa7410812d12e23f
SeongJae Park <sj38.park@gmail.com>
2017-12-13 12:10:54 +0900

    Add process family killer script
    This commit adds a script that kills a process and its entire
    subprocesses.  It first send SIGSTOP to the target processes and then
    kills them one by one from child to parent.  It is as same as
    run_exps.py doing task termination.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ kill_childs_self.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Kill a process and its entire subprocesses
+"""
+
+import os
+import signal
+import subprocess
+import sys
+import time
+
+import exp
+
+if len(sys.argv) < 2:
+    print "Usage: %s <process id>" % sys.argv[0]
+    exit(1)
+
+try:
+    pid = int(sys.argv[1])
+except:
+    print "Wrong pid %s" % sys.argv[1]
+    exit(1)
+
+exp.kill_childs_self(pid)
