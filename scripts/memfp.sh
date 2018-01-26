19ecc70bfb95c5913d7a38fe23bdd2f83bde9373
SeongJae Park <sj38.park@gmail.com>
2018-01-26 11:39:37 +0900

    memfp: Print hierarchical footprint
    Many program is just a wrapper and real workload is done via its
    subprocesses.  In such case, user should know command name of real
    workload to use memfp properly.  This command modifies memfp to
    automatically identify every subprocesses of a command name pointed
    processes and show entire memory footprint.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/memfp.sh
+++ scripts/memfp.sh
@@ -9,10 +9,15 @@ then
 fi
 
 COMM=$1
+PID=`ps -eo pid,comm | grep $COMM | head -n 1 | awk '{print $1}'`
 
-echo "vsz	rss	comm"
+echo "vsz	rss	pid	cmd"
 while true;
 do
-	ps -eo vsz,rss,comm | grep $COMM
+	PIDS=`./subprocs.py $PID`
+	for P in $PIDS
+	do
+		ps -o vsz=,rss=,pid=,cmd= -f $P
+	done
 	sleep 1
 done
