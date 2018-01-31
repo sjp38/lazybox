312608c693556322ea67432a5a027583fa1fded6
SeongJae Park <sj38.park@gmail.com>
2018-01-31 17:52:26 +0900

    statof: Print stdev/average to warning message
    statof.py prints out warning message automatically if required statistic
    is stdev and the stdev value is larger than 10% of average.  This commit
    modifies the message to include `stdev/average` so that users can show
    how big the error is in relative to the average value more easily.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/report/statof.py
+++ scripts/report/statof.py
@@ -82,7 +82,8 @@ for lidx in range(len(datas[0])):
                 variance = sum([pow(v - avg, 2) for v in nrs]) / len(nrs)
                 value = math.sqrt(variance)
                 if value > avg / 10:
-                    pr_stderr("[WARNING] stdev %s, avg %s!" % (value, avg))
+                    pr_stderr("[WARNING] stdev %s, avg %s, %.2f stdev/avg!" % (
+                        value, avg, float(value) / avg))
         except ValueError:
             if not all(f == fields[0] for f in fields):
                 print "[Error] different text field! %s" % fields
