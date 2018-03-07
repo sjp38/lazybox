35baf2e69ba33ee01ebf7f2644c2e982ef6dc002
SeongJae Park <sj38.park@gmail.com>
2018-03-07 21:05:50 +0900

    Add a script for cache dropping
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ scripts/dropcaches.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function usage() {
+	echo "Usage: $0 <1-3>"
+	exit 1
+}
+
+if [ $# -ne 1 ]
+then
+	usage
+fi
+
+ARG=$1
+
+if [ $ARG -lt 1 ] || [ $ARG -gt 3 ]
+then
+	usage
+fi
+
+echo $ARG > /proc/sys/vm/drop_caches
