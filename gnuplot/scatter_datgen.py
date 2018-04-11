9066332f9c3d7e286df872dc48f6e72704dbf6c2
SeongJae Park <sj38.park@gmail.com>
2018-04-11 18:09:11 +0900

    scatter_datgen: Remove final newline
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- gnuplot/scatter_datgen.py
+++ gnuplot/scatter_datgen.py
@@ -5,8 +5,9 @@ import random
 keys = ["systemA", "systemB", "systemC"]
 xaxes = range(1,5)
 
-for k in keys:
+for idx, k in enumerate(keys):
     print k
     for x in xaxes:
         print "%d %d" % (x, random.randint(0, 100))
-    print "\n"
+    if idx < len(keys) - 1:
+        print "\n"
