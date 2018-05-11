54243894a6c3a93635b62b10c8846bbf02ec40d2
SeongJae Park <sj38.park@gmail.com>
2018-05-11 20:30:45 +0900

    report: Add `recs_to_diff.py`
    This commit adds a script, `recs_to_diff.py`.  The script convert lines
    of records in a file to sequence of diff from first line value.  For
    example, repetitive `grep pswpin /proc/vmstat >> a_file` can be a target
    of the script.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ scripts/report/recs_to_diff.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+Convert record to diff from first value.  For example, files with content
+```
+pswpin 123
+pswpin 124
+pswpin 126
+```
+
+Will be converted to
+```
+0 0
+1 1
+2 2
+```
+"""
+
+import sys
+
+if len(sys.argv) < 2:
+    print "Usage: %s <record file>" % sys.argv[0]
+    exit(1)
+
+filepath=sys.argv[1]
+
+with open(filepath, 'r') as f:
+    first_val = 0
+    for idx, line in enumerate(f):
+        if idx == 0:
+            first_val = int(line.split()[1])
+        print idx, int(line.split()[1]) - first_val
