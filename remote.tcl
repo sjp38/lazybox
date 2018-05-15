e53b73995701782dc0080947d5056af11f78297c
hjlee <hyojeong5663@gmail.com>
2018-05-15 17:21:39 +0900

    remote.tcl: Remove duplicated code
    Signed-off-by: hjlee <hyojeong5663@gmail.com>

--- remote.tcl
+++ remote.tcl
@@ -25,18 +25,7 @@ proc remote_sudocmd_registered {username target ssh_port password cmds} {
 }
 
 proc remote_sudoercmd {username target ssh_port password cmds} {
-	spawn ssh -t -p $ssh_port $username@$target bash -c '$cmds'
-
-	# for ssh password
-	expect "*password*"
-	send "$password\r"
-
-	# for sudo command
-	expect "*password*"
-	send "$password\r"
-
-	# wait for completion of ssh
-	expect eof
+	remote_cmd_n_password $username $target $ssh_port $password $cmds 1
 }
 
 # Do remote command which requires n-time password input
