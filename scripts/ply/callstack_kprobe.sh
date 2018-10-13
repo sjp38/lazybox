fc40aad45e2ab7540da880ffdace3c18f6103ae4
SeongJae Park <sj38.park@gmail.com>
2018-10-13 11:20:22 +0900

    ply: Add a script for callstack tracing
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ scripts/ply/callstack_kprobe.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Print callstacks and number of fires of a kprobe point
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <kernel function name>"
+	exit 1
+fi
+
+definition=$1
+
+echo "Press Ctrl-C to finish tracing and show result"
+cmd="sudo ply -c 'kprobe:$definition{ @[stack()].count() }'"
+eval $cmd
