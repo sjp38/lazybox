b2e23843fc775587ae46cdf4eed7011da46e665c
SeongJae Park <sj38.park@gmail.com>
2018-10-16 00:29:56 +0900

    ply: Add a script for total execution time
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ scripts/ply/xtime_kprobe.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Print total execution time of functions having kprobe and kretprobe
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <target>"
+	exit 1
+fi
+
+TARGET=$1
+
+cmd="sudo ply -c \
+'kprobe:$TARGET
+{
+	@start[tid()] = nsecs();
+}
+
+kretprobe:$TARGET / @start[tid()] /
+{
+	latency = nsecs() - @start[tid()];
+	@xtimes[tid()] = @xtimes[tid()] + latency;
+	@start[tid()] = nil;
+}'"
+
+eval $cmd
