572da98c1830273413cd3c6641c917bce299dc88
SeongJae Park <sj38.park@gmail.com>
2018-10-17 14:39:44 +0900

    hwinfo: Use floating point for number of threads per cpu
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/hwinfo/nr_thrspercpu.sh
+++ scripts/hwinfo/nr_thrspercpu.sh
@@ -7,4 +7,4 @@ pushd $BINDIR > /dev/null
 NR_THRS=`./nr_hwthrs.sh`
 NR_CPUS=$((`./nr_cpusocks.sh` * `./nr_cpuspersock.sh`))
 
-echo $(($NR_THRS / $NR_CPUS))
+echo $NR_THRS $NR_CPUS | awk '{print $1 / $2}'
