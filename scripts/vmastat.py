533a653195e84b0f0d42292b1efe6efe923309d1
SeongJae Park <sj38.park@gmail.com>
2018-12-06 09:17:48 +0900

    vmastat: Use a map for pid and number of vmas
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/vmastat.py
+++ scripts/vmastat.py
@@ -11,26 +11,26 @@ verbose = args.verbose
 res = subprocess.check_output("ps --no-headers -e -o pid".split())
 pids = res.split()
 
-nr_proc_vmas = []
+nr_vmas_map = {}
 for p in pids:
     try:
         with open("/proc/%s/maps" % p, 'r') as f:
             nr_vmas = 0
             for l in f:
                 nr_vmas += 1
-        nr_proc_vmas.append(nr_vmas)
+        nr_vmas_map[p] = nr_vmas
     except:
-        nr_proc_vmas.append(-1)
+        pass
 
 print "pid\tnr_vmas"
-for i in range(0, len(pids)):
-    print "%s\t%d" % (pids[i], nr_proc_vmas[i])
+for p in nr_vmas_map:
+    print "%s\t%d" % (p, nr_vmas_map[p])
 print
 
-nr_vmas_sorted = sorted([n for n in nr_proc_vmas if n != -1])
+nr_vmas_sorted = sorted(nr_vmas_map.values())
 l = len(nr_vmas_sorted)
 print "nr_procs: %d" % l
 print "average_nr_vmas: %d" % (sum(nr_vmas_sorted) / l)
 print "min\t25th\t50th\t75th\tmax"
 print "%d\t%d\t%d\t%d\t%d" % (nr_vmas_sorted[0], nr_vmas_sorted[l / 4],
-        nr_vmas_sorted[l / 2], nr_vmas_sorted[l / 4 * 3], nr_vmas_sorted[-1])
+        nr_vmas_sorted[l / 4], nr_vmas_sorted[l / 4 * 3], nr_vmas_sorted[-1])
