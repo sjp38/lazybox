e3ebba7605848c5194ed3a36a91facf91e62128a
SeongJae Park <sj38.park@gmail.com>
2018-12-18 10:19:01 +0900

    workloads/mosbench/psearchy: Add a script for build
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ workloads/mosbench/psearchy/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BINDIR=`dirname $0`
+cd $BINDIR
+
+if [ ! -d ../mosbench/psearchy ]
+then
+	echo "psearchy source code directory not found."
+	exit 1
+fi
+
+if ! dpkg -l | grep -q libdb-dev
+then
+	echo "libdb-dev package is not installed"
+	exit 1
+fi
+
+make -C ../mosbench/psearchy/mkdb all
