7934277738bd6ba43f34c46dbe7a229ee51844a6
SeongJae Park <sj38.park@gmail.com>
2018-12-18 10:11:35 +0900

    Add a script for metis run
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ workloads/mosbench/metis/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BINDIR=`dirname $0`
+cd $BINDIR
+
+if [ ! -f ../mosbench/metis/obj/app/wrmem ]
+then
+	echo "the execution file for the metis is not found."
+	exit 1
+fi
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <number of cores>"
+	exit 1
+fi
+
+NR_CORES=$1
+
+../mosbench/metis/obj/app/wrmem -p $NR_CORES
