5d23f8368d447d7e09995fbb8490304046dddbfd
SeongJae Park <sj38.park@gmail.com>
2018-12-19 10:09:17 +0900

    workloads/mosbench: Checkout fixed version
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- workloads/mosbench/fetch-mosbench.sh
+++ workloads/mosbench/fetch-mosbench.sh
@@ -7,3 +7,6 @@ then
 fi
 
 git clone https://github.com/sjp38/mosbench
+cd mosbench
+# Check out latest version as of 19 Dec 2018.
+git checkout c250c395fab356ab83413db43bf9844cb4f63d4f	# Mar 4 2013
