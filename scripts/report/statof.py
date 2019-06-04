0dc307dd538970b5669c1b6e6f208f1208d8a420
SeongJae Park <sj38.park@gmail.com>
2019-06-04 13:15:39 +0900

    Use explicit python2.7 shebangs
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/report/statof.py
+++ scripts/report/statof.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 program_decr = """
 Make user specified status (average, min, max, stdev) from single or multiple
--- workloads/cloudsuite/webserving/parse_summary.py
+++ workloads/cloudsuite/webserving/parse_summary.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 """
 Receive summary.xml from stdin and print out essential fields
