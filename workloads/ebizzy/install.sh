c4ace77f614aaa8af890bf4e2e35b2628e713914
SeongJae Park <sj38.park@gmail.com>
2019-06-06 08:28:01 +0900

    workload: Add ebizzy install script
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ workloads/ebizzy/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -f ebizzy-0.3/ebizzy ]
+then
+	echo "ebizzy-0.3 directory already exists!"
+	exit 1
+fi
+
+wget http://www.phoronix-test-suite.com/benchmark-files/ebizzy-0.3.tar.gz
+tar xvf ebizzy-0.3.tar.gz
+cd ebizzy-0.3
+cc -pthread -lpthread -O3 -o ebizzy ebizzy.c
