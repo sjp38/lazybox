8904a11be707a61f69102d51e14c96c080571076
SeongJae Park <sj38.park@gmail.com>
2020-01-02 18:21:21 +0100

    scripts/cache_hierarchy: Clean up code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/cache_hierarchy.sh
+++ scripts/cache_hierarchy.sh
@@ -2,15 +2,17 @@
 
 CACHEDIR=/sys/devices/system/cpu/cpu0/cache/index
 
-for level in 0 1 2 3; do
+for level in 0 1 2 3;
+do
 	echo "Level $level"
 	echo "======="
 	echo ""
 
-	for file in type size ways_of_associativity number_of_sets coherency_line_size; do
+	for file in type size ways_of_associativity number_of_sets \
+		coherency_line_size;
+	do
 		printf "$file: %s\n" `cat $CACHEDIR$level/$file`
 	done
 
 	printf "\n\n"
-
 done
