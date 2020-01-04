22eabee27081f2f9273fb2e26daf0990ff2b15cf
SeongJae Park <sj38.park@gmail.com>
2020-01-04 12:27:06 +0100

    kill_childs_self: Use Python3
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- kill_childs_self.py
+++ kill_childs_self.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 """
 Kill a process and its entire subprocesses
@@ -13,13 +13,13 @@ import time
 import exp
 
 if len(sys.argv) < 2:
-    print "Usage: %s <process id>" % sys.argv[0]
+    print("Usage: %s <process id>" % sys.argv[0])
     exit(1)
 
 try:
     pid = int(sys.argv[1])
 except:
-    print "Wrong pid %s" % sys.argv[1]
+    print("Wrong pid %s" % sys.argv[1])
     exit(1)
 
 exp.kill_childs_self(pid)
