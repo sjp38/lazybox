5324e29ee6fdc8f69a2a60983e30bfbd673d91c2
SeongJae Park <sj38.park@gmail.com>
2020-01-05 08:51:34 +0100

    gnuplot/scatter-yerr_datgen: Use Python3
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- gnuplot/scatter-yerr_datgen.py
+++ gnuplot/scatter-yerr_datgen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 import random
 
@@ -6,8 +6,8 @@ keys = ["systemA", "systemB", "systemC"]
 xaxes = range(1,5)
 
 for idx, k in enumerate(keys):
-    print k
+    print(k)
     for x in xaxes:
-        print "%d %d %d" % (x, random.randint(0, 100), random.randint(0,30))
+        print("%d %d %d" % (x, random.randint(0, 100), random.randint(0,30)))
     if idx < len(keys) - 1:
-        print "\n"
+        print("\n")
