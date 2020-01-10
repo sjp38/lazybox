7c14ad118bbd040b34384f7fb15da30077b36b13
SeongJae Park <sj38.park@gmail.com>
2020-01-10 17:23:56 +0100

    report/memfree_to_used: Use Python3
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/report/memfree_to_used.py
+++ scripts/report/memfree_to_used.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 import sys
 
 if len(sys.argv) < 2:
-    print "Usage: %s <memfree file>" % sys.argv[0]
+    print("Usage: %s <memfree file>" % sys.argv[0])
     exit(1)
 
 filepath = sys.argv[1]
@@ -15,4 +15,4 @@ with open(filepath, 'r') as f:
 
 initfree = free_values[0]
 for idx, v in enumerate(free_values):
-    print idx, initfree - v
+    print(idx, initfree - v)
