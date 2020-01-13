d8ce80e5d0a42b53c391b005fcba30fd3fff2ab2
SeongJae Park <sj38.park@gmail.com>
2020-01-13 17:19:28 +0100

    report/yzoom: Use Python3
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/report/yzoom.py
+++ scripts/report/yzoom.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 import sys
 
 if len(sys.argv) < 3:
-    print "Usage: %s <min> <max>" % sys.argv[0]
+    print("Usage: %s <min> <max>" % sys.argv[0])
     sys.exit(1)
 
 ymin = int(sys.argv[1])
@@ -13,4 +13,4 @@ for l in sys.stdin:
     # We support hexadecimal with prefix '0x'
     yval = int(l.split()[1], 0)
     if yval > ymin and yval < ymax:
-        print l.strip()
+        print(l.strip())
