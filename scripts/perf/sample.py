ac7bbeb9e6f1e95811728030c270840a1b2abf15
SeongJae Park <sj38.park@gmail.com>
2020-01-25 10:06:23 +0100

    scripts/perf/sample: Use python3
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/perf/sample.py
+++ scripts/perf/sample.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 import sys
 
 if len(sys.argv) != 2:
-    print "Usage: %s <interval between lines>" % sys.argv[0]
+    print("Usage: %s <interval between lines>" % sys.argv[0])
     sys.exit(1)
 interval=int(sys.argv[1])
 
 i = 0
 for l in sys.stdin:
     if i % interval == 0:
-        print l.strip()
+        print(l.strip())
     i += 1
