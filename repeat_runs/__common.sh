a0cc459da6e84c47014f165c6df8e9f4360690f4
SeongJae Park <sj38.park@gmail.com>
2020-02-25 18:58:03 +0000

    repeat_runs/__common: Clean up code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- repeat_runs/__common.sh
+++ repeat_runs/__common.sh
@@ -5,21 +5,13 @@ BINDIR=`dirname $0`
 LBX=$BINDIR'/../'
 ODIR_ROOT=$PWD'/results'
 
+VARIANTS="orig"
+REPEATS=1
+PARSED='parsed'
+
 if [ -z "$CFG" ]
 then
 	CFG=$BINDIR/examples/example.config
 fi
 
 source $CFG
-
-if [ -z "$VARIANTS" ]
-then
-	VARIANTS="orig"
-fi
-
-if [ -z "$REPEATS" ]
-then
-	REPEATS=1
-fi
-
-PARSED='parsed'
