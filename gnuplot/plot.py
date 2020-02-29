b3208a32f1a8896611d605e5d7db96e52c06f36e
SeongJae Park <sj38.park@gmail.com>
2020-02-29 10:31:34 +0100

    gnuplot/plot: Cleanup code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- gnuplot/plot.py
+++ gnuplot/plot.py
@@ -6,7 +6,7 @@ import subprocess
 import sys
 import tempfile
 
-def main():
+def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--stdin', '-s', action='store_true',
             help='read data from stdin')
@@ -22,7 +22,51 @@ def main():
     parser.add_argument('--ylog', action='store_true',
             help='plot y axis in logscale')
     parser.add_argument('out', metavar='<file>', help='output file')
-    args = parser.parse_args()
+    return parser.parse_args()
+
+def gen_gp_cmd(data_path, nr_recs, nr_cols, plot_type, out_extension, output,
+        xtitle, ytitle, xlog, ylog):
+    cmdlines = []
+    cmdlines.append("""
+    load "lzstyle.gp";
+
+    set autoscale;""")
+
+    if plot_type == 'clustered_boxes':
+        cmdlines.append("""
+        set style data histogram;
+        set style histogram cluster gap 2;""")
+
+    cmdlines.append("""
+    set term %s;
+    set output '%s';
+    """ % (out_extension, output))
+    if xtitle:
+        cmdlines.append("set xlabel '%s';" % xtitle)
+    if ytitle:
+        cmdlines.append("set ylabel '%s';" % ytitle)
+
+    log = ''
+    if xlog:
+        log += 'x'
+    if ylog:
+        log += 'y'
+    if log:
+        cmdlines.append("set logscale %s;" % log)
+
+    if plot_type == 'scatter':
+        cmdlines.append("""
+        plot for [idx=0:%s] '%s' index idx using 1:2 with linespoints title columnheader(1);
+        """ % (nr_recs, data_path))
+    elif plot_type == 'clustered_boxes':
+        cmdlines.append("""
+        plot '%s' using 2:xtic(1) title column, for [i=3:%s] '' using i title column;
+        """ % (tmp_path, nr_cols))
+
+    return '\n'.join(cmdlines)
+
+def main():
+    args = get_args()
 
     if not args.stdin and not args.file:
         print('no data source specified')
@@ -39,55 +83,19 @@ def main():
         f = sys.stdin
     elif args.file:
         f = open(args.file, 'r')
+    data = f.read()
+    f.close()
 
     tmp_path = tempfile.mkstemp()[1]
-    data_file = open(tmp_path, 'w')
-
-    data = f.read()
-    data_file.write(data)
-    data_file.close()
+    with open(tmp_path, 'w') as f:
+        f.write(data)
 
     nr_cols = len(data.split('\n')[0].split())
     nr_recs = len(data.split('\n\n')) - 1
 
-    cmdlines = []
-    cmdlines.append("""
-    load "lzstyle.gp";
-
-    set autoscale;""")
-
-    if plot_type == 'clustered_boxes':
-        cmdlines.append("""
-        set style data histogram;
-        set style histogram cluster gap 2;""")
-
-    cmdlines.append("""
-    set term %s;
-    set output '%s';
-    """ % (out_extension, output))
-    if args.xtitle:
-        cmdlines.append("set xlabel '%s';" % args.xtitle)
-    if args.ytitle:
-        cmdlines.append("set ylabel '%s';" % args.ytitle)
-
-    log = ''
-    if args.xlog:
-        log += 'x'
-    if args.ylog:
-        log += 'y'
-    if log:
-        cmdlines.append("set logscale %s;" % log)
-
-    if plot_type == 'scatter':
-        cmdlines.append("""
-        plot for [idx=0:%s] '%s' index idx using 1:2 with linespoints title columnheader(1);
-        """ % (nr_recs, tmp_path))
-    elif plot_type == 'clustered_boxes':
-        cmdlines.append("""
-        plot '%s' using 2:xtic(1) title column, for [i=3:%s] '' using i title column;
-        """ % (tmp_path, nr_cols))
-
-    gnuplot_cmd = '\n'.join(cmdlines)
+    gnuplot_cmd = gen_gp_cmd(tmp_path, nr_recs, nr_cols, plot_type,
+            out_extension, output, args.xtitle, args.ytitle, args.xlog,
+            args.ylog)
 
     subprocess.call(['gnuplot', '-e', gnuplot_cmd])
     os.remove(tmp_path)
