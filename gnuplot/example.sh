36ab5bc6c24c170abfe6be03a69d838b7b77a87b
SeongJae Park <sj38.park@gmail.com>
2020-03-03 08:04:41 +0100

    gnuplot/example: Stop if plotting fails
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- gnuplot/example.sh
+++ gnuplot/example.sh
@@ -3,14 +3,18 @@
 ODIR="example_plots"
 mkdir -p $ODIR
 
-OPTS="--size 5,1 --title 'example' \
-	--xrange [0:10] --yrange [0:*] --ylog --xtics_rotate 45 \
+OPTS="--size 7,3 --title 'example' \
+	--xrange [0:10] --yrange [0:*] --ylog --xtics_rotate -45 \
 	--gnuplot_cmds"
 
-./scatter_datgen.py | ./plot.py $ODIR/scatter.pdf --stdin $OPTS
+./scatter_datgen.py | ./plot.py $ODIR/scatter.pdf --stdin $OPTS \
+	--font 'times new roman,20' \
+&& \
 ./scatter-yerr_datgen.py | ./plot.py $ODIR/scatter-yerr.pdf --stdin \
-	--type scatter-yerr $OPTS
+	--type scatter-yerr $OPTS --xlog --xtitle "examle x title" \
+&& \
 ./clustered_box_datgen.py | ./plot.py $ODIR/cluster.pdf --stdin \
-	--type clustered_boxes $OPTS
+	--type clustered_boxes $OPTS --ytitle "example y title" \
+&& \
 ./clustered_box-yerr_datgen.py | ./plot.py $ODIR/cluster-yerr.pdf --stdin \
 	--type clustered_boxes-yerr $OPTS
