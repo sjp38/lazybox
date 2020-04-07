ad303d3c42f37c1b3ea603cdb7554e2bd6d1fd97
SeongJae Park <sj38.park@gmail.com>
2020-04-07 07:00:55 +0200

    gnuplot/plot_stdin.sh: Use plot.py internally
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- gnuplot/plot_stdin.sh
+++ gnuplot/plot_stdin.sh
@@ -4,41 +4,34 @@ if [ $# -lt 3 ]
 then
 	echo "Usage: $0 <chart type> <x label> <y label> [logscale]"
 	echo " supported chart types are:"
-	echo "         scatter, scatter-yerr, clustered_box, heatmap"
+	echo "         scatter, scatter-yerr, clustered_boxes, heatmap"
 	exit 1
 fi
 
-SCRIPT_DIR=`dirname $0`
+BINDIR=`dirname $0`
 
 CHART_TYPE=$1
 XLABEL=$2
 YLABEL=$3
 LOGSCALE=$4
 
-export GNUPLOT_LIB=$SCRIPT_DIR
-
-NR_TRY=0
+export GNUPLOT_LIB=$BINDIR
 
 TMPFILE=`mktemp /tmp/lbx-gnuplot.XXX`
 
 cat /dev/stdin > $TMPFILE
 
-function nr_records {
-	awk 'BEGIN{RS="\n\n\n";}{print NR;}' $1 | wc -l
-}
-
-# index start from 0
-NR_IDXS=$((`nr_records $TMPFILE` - 1))
-NR_COLS=`cat $TMPFILE | tail -n 1 | awk '{print NF}'`
-
-if [ $NR_COLS -lt 2 ]
+if [ "$LOGSCALE" = "x" ]
 then
-	echo "Number of columns is $NR_COLS (< 2).  Maybe something wrong."
-	exit 1
+	LOGSCALE="--xlog"
+elif [ "$LOGSCALE" = "y" ]
+then
+	LOGSCALE="--ylog"
+elif [ "$LOGSCALE" = "xy" ]
+then
+	LOGSCALE="--xlog --ylog"
 fi
 
-gnuplot -e "DATA='$TMPFILE'; NR_IDXS='$NR_IDXS'; NR_COLS='$NR_COLS'; \
-		XLABEL='$XLABEL'; YLABEL='$YLABEL'; \
-		LOGSCALE='$LOGSCALE'" ./$CHART_TYPE.gp
-
+$BINDIR/plot.py --file $TMPFILE --type $CHART_TYPE \
+	--xtitle $XLABEL --ytitle $YLABEL  $LOGSCALE
 rm $TMPFILE
