21b6c9886661cf43a4e9c6a4206e660c888c469e
SeongJae Park <sj38.park@gmail.com>
2020-04-23 06:46:40 +0200

    memcg_mspike: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/memcg_mspike.sh
+++ scripts/memcg_mspike.sh
@@ -8,14 +8,14 @@ then
 	exit 1
 fi
 
-if [ ! -f $HOME/memwalk/memwalk ]
+if [ ! -f "$HOME/memwalk/memwalk" ]
 then
 	echo "Install memwalk at $HOME/memwalk/memwalk first."
 	echo "You can clone it from https://github.com/sjp38/memwalk"
 	exit 1
 fi
 
-BINDIR=`dirname $0`
+BINDIR=$(dirname "$0")
 cd $BINDIR
 
 MWALK=$HOME/memwalk/memwalk
@@ -23,15 +23,15 @@ MWALK=$HOME/memwalk/memwalk
 SZ_SPIKE=$(($1 * 1024 * 1024))
 INTERVAL=$2
 
-MEMCG=`cat /proc/$$/cgroup | grep memory | awk -F: '{print $3}'`
+MEMCG=$(grep memory /proc/$$/cgroup | awk -F: '{print $3}')
 MEMCG=/sys/fs/cgroup/memory/$MEMCG
-MEMLIM=`cat $MEMCG/memory.limit_in_bytes`
+MEMLIM=$(cat "$MEMCG"/memory.limit_in_bytes)
 
 while :;
 do
-	sleep $INTERVAL
-	MEM_IN_USE=`cat $MEMCG/memory.usage_in_bytes`
-	TO_CONSUME=$(($MEMLIM - $MEM_IN_USE + $SZ_SPIKE))
+	sleep "$INTERVAL"
+	MEM_IN_USE=$(cat "$MEMCG"/memory.usage_in_bytes)
+	TO_CONSUME=$((MEMLIM - MEM_IN_USE + SZ_SPIKE))
 	echo "consume $TO_CONSUME bytes"
 	$MWALK $TO_CONSUME 64 0 -q
 done
