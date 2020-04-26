dff3dbdaf72de9419f85ddd49ff9d618c10a72a3
SeongJae Park <sj38.park@gmail.com>
2020-04-26 08:51:46 +0200

    scripts/run_memcg_lim: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/run_memcg_lim.sh
+++ scripts/run_memcg_lim.sh
@@ -13,7 +13,7 @@ COMM=$2
 
 MEMCG_ORIG_DIR=/sys/fs/cgroup/memory/
 MEMCG_DIR=/sys/fs/cgroup/memory/run_memcg_lim_$USER
-sudo mkdir $MEMCG_DIR
+sudo mkdir "$MEMCG_DIR"
 sudo bash -c "echo $$ > $MEMCG_DIR/tasks"
 sudo bash -c "echo $MEMLIM > $MEMCG_DIR/memory.limit_in_bytes"
 
@@ -21,8 +21,8 @@ echo "COMM: $COMM"
 
 eval "$COMM"
 
-while read pid; do
+while read -r pid; do
 	sudo bash -c "echo $pid > $MEMCG_ORIG_DIR/tasks"
-done < $MEMCG_DIR/tasks
+done < "$MEMCG_DIR"/tasks
 
-sudo rmdir $MEMCG_DIR
+sudo rmdir "$MEMCG_DIR"
