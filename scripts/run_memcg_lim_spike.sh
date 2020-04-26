fdc2d4d7a37bd33a628104f9fe9296cdf48bff39
SeongJae Park <sj38.park@gmail.com>
2020-04-26 08:57:38 +0200

    run_memcg_lim_spike: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/run_memcg_lim_spike.sh
+++ scripts/run_memcg_lim_spike.sh
@@ -10,8 +10,8 @@ then
 	exit 1
 fi
 
-BINDIR=`dirname $0`
-cd $BINDIR
+BINDIR=$(dirname "$0")
+cd "$BINDIR" || (echo 'failed moving to bindir'; exit 1)
 
 MEMLIM=$(($1 * 1024 * 1024))
 SZ_SPIKE=$2
@@ -20,11 +20,11 @@ COMM=$4
 
 MEMCG_ORIG_DIR=/sys/fs/cgroup/memory/
 MEMCG_DIR=/sys/fs/cgroup/memory/run_memcg_lim_$USER
-sudo mkdir $MEMCG_DIR
+sudo mkdir "$MEMCG_DIR"
 sudo bash -c "echo $$ > $MEMCG_DIR/tasks"
 sudo bash -c "echo $MEMLIM > $MEMCG_DIR/memory.limit_in_bytes"
 
-./memcg_mspike.sh $SZ_SPIKE $SPIKE_INTERVAL &
+./memcg_mspike.sh "$SZ_SPIKE" "$SPIKE_INTERVAL" &
 
 echo "COMM: $COMM"
 
@@ -34,8 +34,8 @@ killall memcg_mspike.sh
 
 
 
-while read pid; do
+while read -r pid; do
 	sudo bash -c "echo $pid > $MEMCG_ORIG_DIR/tasks"
-done < $MEMCG_DIR/tasks
+done < "$MEMCG_DIR"/tasks
 
-sudo rmdir $MEMCG_DIR
+sudo rmdir "$MEMCG_DIR"
