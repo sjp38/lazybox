92922016cbc1a54f2a933b8a65150adae8911026
SeongJae Park <sj38.park@gmail.com>
2020-05-05 07:03:22 +0200

    scripts/turn_thp: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/turn_thp.sh
+++ scripts/turn_thp.sh
@@ -6,9 +6,9 @@ if [ $# -ne 1 ];
 then
 	echo "Usage: $0 <always|madvise|never>"
 	echo ""
-	echo "Current status: " $(cat $SYSFS_THP_ENABLED)
+	echo "Current status: $(cat $SYSFS_THP_ENABLED)"
 	echo ""
 	exit 1
 fi
 
-echo $1 > $SYSFS_THP_ENABLED
+echo "$1" > $SYSFS_THP_ENABLED
