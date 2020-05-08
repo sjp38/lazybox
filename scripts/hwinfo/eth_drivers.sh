b4b4844ef1e16bbe65ee1c3b438bd52d345b0f8d
SeongJae Park <sj38.park@gmail.com>
2020-05-08 07:20:48 +0200

    hwinfo/eth_drivers: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/hwinfo/eth_drivers.sh
+++ scripts/hwinfo/eth_drivers.sh
@@ -4,16 +4,16 @@ echo "Dev	MAC			Driver	State"
 
 for e in /sys/class/net/*
 do
-	dev=`basename $e`
-	driver=`readlink $e/device/driver/module`
-	if [ $driver ]
+	dev=$(basename "$e")
+	driver=$(readlink "$e"/device/driver/module)
+	if [ "$driver" ]
 	then
-		driver=`basename $driver`
+		driver=$(basename "$driver")
 	else
 		driver="none"
 	fi
-	addr=`cat $e/address`
-	operstate=`cat $e/operstate`
+	addr=$(cat "$e/address")
+	operstate=$(cat "$e/operstate")
 
-	printf "$dev\t$addr\t$driver\t$operstate\n"
+	printf "%s\t%s\t%s\t%s\n" "$dev" "$addr" "$driver" "$operstate"
 done
