c589cf9bd9157c054117a1518985da8b2ade09e1
SeongJae Park <sj38.park@gmail.com>
2020-05-14 06:40:27 +0200

    hwinfo/sz_mem: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/hwinfo/sz_mem.sh
+++ scripts/hwinfo/sz_mem.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sz=`grep "^MemTotal:" /proc/meminfo | awk '{print $2}'`
+sz=$(grep "^MemTotal:" /proc/meminfo | awk '{print $2}')
 unit=KiB
 
-if [ $sz -gt 1024 ]
+if [ "$sz" -gt 1024 ]
 then
 	sz=$((sz / 1024))
 	unit=MiB
 fi
 
-if [ $sz -gt 1024 ]
+if [ "$sz" -gt 1024 ]
 then
 	sz=$((sz / 1024))
 	unit=GiB
