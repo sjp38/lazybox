73b53c682d4df618cd4bcc427b4f4ae37fcbaecf
SeongJae Park <sj38.park@gmail.com>
2020-05-14 06:38:38 +0200

    hwinfo/nr_thrspercpu: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/hwinfo/nr_thrspercpu.sh
+++ scripts/hwinfo/nr_thrspercpu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-BINDIR=`dirname $0`
+BINDIR=$(dirname "$0")
 
-pushd $BINDIR > /dev/null
+pushd "$BINDIR" > /dev/null
 
-NR_THRS=`./nr_hwthrs.sh`
-NR_CPUS=$((`./nr_cpusocks.sh` * `./nr_cpuspersock.sh`))
+NR_THRS=$(./nr_hwthrs.sh)
+NR_CPUS=$(( $(./nr_cpusocks.sh) * $(./nr_cpuspersock.sh)))
 
-echo $NR_THRS $NR_CPUS | awk '{print $1 / $2}'
+echo "$NR_THRS" "$NR_CPUS" | awk '{print $1 / $2}'
