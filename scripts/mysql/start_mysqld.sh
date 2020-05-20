65df5aa1f2768d0a0abc4e5d707b1cc36d373637
SeongJae Park <sj38.park@gmail.com>
2020-05-20 06:10:01 +0200

    mysql/start_mysqld: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/mysql/start_mysqld.sh
+++ scripts/mysql/start_mysqld.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-BINDIR=`dirname $0`
+BINDIR=$(dirname "$0")
 
-pushd $BINDIR > /dev/null
+pushd "$BINDIR" > /dev/null
 
-NOUSE=`sudo whoami`
 sudo -u mysql /usr/local/mysql/bin/mysqld_safe --user=mysql &
 
 ../wait_workof.sh mysqld
