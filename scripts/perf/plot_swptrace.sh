a06df246b8c91ba4a360046ebd32bdb8d0af794d
SeongJae Park <sj38.park@gmail.com>
2020-05-22 06:40:16 +0200

    plot_swptrace: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/perf/plot_swptrace.sh
+++ scripts/perf/plot_swptrace.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-BINDIR=`dirname $0`
+BINDIR=$(dirname "$0")
 
 MIDFILE=perf-scr.out
 MAX_SAMPLES=1000
 
 perf script > $MIDFILE
-NR_SAMPLES=`cat $MIDFILE | wc -l`
-if [ $NR_SAMPLES -gt $MAX_SAMPLES ]
+NR_SAMPLES=$(wc -l < "$MIDFILE")
+if [ "$NR_SAMPLES" -gt "$MAX_SAMPLES" ]
 then
 	echo "Too big ($NR_SAMPLES) data.  Shrink to $MAX_SAMPLES data"
-	$BINDIR/sample.py $(($NR_SAMPLES / $MAX_SAMPLES)) < $MIDFILE > $MIDFILE.tmp
+	"$BINDIR"/sample.py $(("$NR_SAMPLES" / "$MAX_SAMPLES")) < $MIDFILE \
+		> $MIDFILE.tmp
 	mv $MIDFILE.tmp $MIDFILE
 fi
 
 parse_perfout() {
 	echo "Swpin"
-	cat $MIDFILE | grep swpin | sed -e 's/://' | sed -e 's/page=//' |
+	grep swpin "$MIDFILE" | sed -e 's/://' | sed -e 's/page=//' |
 		awk '{print $4 " " $7}'
 	echo
 	echo
 	echo "swpout"
-	cat $MIDFILE | grep swpout | sed -e 's/://' | sed -e 's/page=//' |
+	grep swpout "$MIDFILE" | sed -e 's/://' | sed -e 's/page=//' |
 		awk '{print $4 " " $7}'
 }
 
 parse_perfout |
-	$BINDIR/../../gnuplot/plot_stdin.sh scatter-dot \
+	"$BINDIR"/../../gnuplot/plot_stdin.sh scatter-dot \
 		"Time (seconds)" "Page Frame"
