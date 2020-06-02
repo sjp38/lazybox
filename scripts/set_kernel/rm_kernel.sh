a49b42bc8db2b4e5a3e28b67d9b85c0f85923e93
SeongJae Park <sj38.park@gmail.com>
2020-06-02 06:59:11 +0200

    rm_kernel: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/set_kernel/rm_kernel.sh
+++ scripts/set_kernel/rm_kernel.sh
@@ -6,10 +6,10 @@ then
 	exit 1
 fi
 
-rm /boot/vmlinuz-$1
-rm /boot/initrd.img-$1
-rm /boot/System.map-$1
-rm /boot/config-$1
-rm -fr /lib/modules/$1
-rm /var/lib/initramfs-tools/$1
+rm "/boot/vmlinuz-$1"
+rm "/boot/initrd.img-$1"
+rm "/boot/System.map-$1"
+rm "/boot/config-$1"
+rm -fr "/lib/modules/$1"
+rm "/var/lib/initramfs-tools/$1"
 update-grub2
