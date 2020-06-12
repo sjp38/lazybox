2a288b39005995ed5a26fa43a0b4591c54a722f0
SeongJae Park <sj38.park@gmail.com>
2020-06-12 06:58:34 +0200

    repeat_runs/post: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- repeat_runs/post.sh
+++ repeat_runs/post.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-BINDIR=`dirname $0`
+BINDIR=$(dirname "$0")
 
-source $BINDIR/__common.sh
+source "$BINDIR/__common.sh"
 
 for exp in $EXPERIMENTS
 do
-	exp_basename=`basename $exp`
+	exp_basename=$(basename "$exp")
 	for v in $VARIANTS
 	do
-		for d in $ODIR_ROOT/$exp_basename/$v/0*
+		for d in "$ODIR_ROOT/$exp_basename/$v/"0*
 		do
-			$BINDIR/_parse.sh $exp $d
+			"$BINDIR"/_parse.sh "$exp" "$d"
 		done
 	done
 done
 
 for exp in $EXPERIMENTS
 do
-	exp_basename=`basename $exp`
+	exp_basename=$(basename "$exp")
 	for v in $VARIANTS
 	do
-		ODIR=$ODIR_ROOT/$exp_basename/$v
-		$BINDIR/_stat.sh $exp $ODIR
+		ODIR="$ODIR_ROOT/$exp_basename/$v"
+		"$BINDIR/"_stat.sh "$exp" "$ODIR"
 	done
 done
