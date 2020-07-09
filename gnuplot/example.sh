7a7201b23b00fae821f2836c9458aa43376b55a0
SeongJae Park <sj38.park@gmail.com>
2020-07-09 07:00:43 +0200

    gnuplot/example.sh: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- gnuplot/example.sh
+++ gnuplot/example.sh
@@ -3,18 +3,20 @@
 ODIR="example_plots"
 mkdir -p $ODIR
 
-OPTS="--size 7,3 --title 'example' \
-	--xrange [0:10] --yrange [0:*] --ylog --xtics_rotate -45 \
-	--gnuplot_cmds"
+OPTS="--size 7,3 --title 'example' "
+OPTS+="--xrange [0:10] --yrange [0:*] --ylog --xtics_rotate -45 "
+OPTS+="--gnuplot_cmds"
 
-./scatter_datgen.py | ./plot.py $ODIR/scatter.pdf $OPTS \
+OPTS=($OPTS)
+
+./scatter_datgen.py | ./plot.py $ODIR/scatter.pdf "${OPTS[@]}" \
 	--font 'times new roman,20' \
 && \
 ./scatter-yerr_datgen.py | ./plot.py $ODIR/scatter-yerr.pdf \
-	--type scatter-yerr $OPTS --xlog --xtitle "examle x title" \
+	--type scatter-yerr "${OPTS[@]}" --xlog --xtitle "examle x title" \
 && \
 ./clustered_box_datgen.py | ./plot.py $ODIR/cluster.pdf \
-	--type clustered_boxes $OPTS --ytitle "example y title" \
+	--type clustered_boxes "${OPTS[@]}" --ytitle "example y title" \
 && \
 ./clustered_box-yerr_datgen.py | ./plot.py $ODIR/cluster-yerr.pdf \
-	--type clustered_boxes-yerr $OPTS
+	--type clustered_boxes-yerr "${OPTS[@]}"
