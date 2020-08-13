25f14373870402045c05d9e5ed0619dbbbc584e3
SeongJae Park <sj38.park@gmail.com>
2020-08-13 07:52:30 +0200

    workload/gcma/gcma.sh: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- workloads/gcma/gcma.sh
+++ workloads/gcma/gcma.sh
@@ -7,7 +7,7 @@ then
 	exit 1
 fi
 
-WORKING_DIR=`dirname $0`
+WORKING_DIR=$(dirname "$0")
 
 DEBUGFS_ROOT="/sys/kernel/debug"
 DEBUGFS=$DEBUGFS"/cma_eval"
@@ -15,22 +15,22 @@ NR_CMA_REQ=$1
 NR_ALLOC_PAGES=$2
 
 echo "[[ Test Start ]]"
-echo "kernel version: `uname -r`"
-echo "kernel param: \"`cat /proc/cmdline`\""
-echo "date: `date`"
+echo "kernel version: $(uname -r)"
+echo "kernel param: \"$(cat /proc/cmdline)\""
+echo "date: $(date)"
 echo ""
 
-pushd $WORKING_DIR
+pushd "$WORKING_DIR"
 
 echo "[before stat]"
 ./gcma_stat.sh
 
-for i in $(seq 1 $NR_CMA_REQ)
+for i in $(seq 1 "$NR_CMA_REQ")
 do
 	for NR in $NR_ALLOC_PAGES
 	do
 		sleep 2
-		echo $NR > $DEBUGFS_ROOT/cma_eval/eval
+		echo "$NR" > "$DEBUGFS_ROOT"/cma_eval/eval
 	done
 done
 
@@ -44,7 +44,7 @@ cat $DEBUGFS_ROOT/cma_eval/res.hist
 
 echo ""
 echo "[after stat]"
-$WORKING_DIR/gcma_stat.sh
+"$WORKING_DIR"/gcma_stat.sh
 
 popd
 
