85efa5908e20e0a38c78aa62118b65738677741c
SeongJae Park <sj38.park@gmail.com>
2021-01-27 07:26:30 +0100

    repeat_runs/examples/memprofile/parsers: Fix some shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- repeat_runs/examples/memprofile/parsers/commlog.sh
+++ repeat_runs/examples/memprofile/parsers/commlog.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep real $1/commlog > $2/perf
+grep real "$1/commlog" > "$2/perf"
--- repeat_runs/examples/memprofile/parsers/memfree.sh
+++ repeat_runs/examples/memprofile/parsers/memfree.sh
@@ -2,13 +2,13 @@
 
 LBX=$HOME/lazybox
 
-$LBX/scripts/report/memfree_to_used.py $1/memfree > $2/memused
+"$LBX/scripts/report/memfree_to_used.py" "$1/memfree" > "$2/memused"
 
 MFTOT=0
 NR_MF=0
-for mf in `awk '{print $2}' $2/memused`
+for mf in $(awk '{print $2}' "$2/memused")
 do
-	MFTOT=$(($MFTOT + $mf))
-	NR_MF=$(($NR_MF + 1))
+	MFTOT=$((MFTOT + mf))
+	NR_MF=$((NR_MF + 1))
 done
-echo "memused.avg: " $(($MFTOT / $NR_MF)) > $2/memused.avg
+echo "memused.avg: " $((MFTOT / NR_MF)) > "$2/memused.avg"
