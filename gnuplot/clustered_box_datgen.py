e35bd57a7725859cb97ae5089c5f96b49ecf4956
SeongJae Park <sj38.park@gmail.com>
2021-10-02 10:26:58 +0200

    clustered_box_datgen: Increase max value limit
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- gnuplot/clustered_box_datgen.py
+++ gnuplot/clustered_box_datgen.py
@@ -8,6 +8,6 @@ xaxes = ["metricA", "metricBB", "metricC", "metricD", "metricE", "metricF"]
 print("\t".join(keys))
 line = ""
 for i in range(len(xaxes)):
-    values = [str(random.randint(0, 100)) for _ in keys[1:]]
+    values = [str(random.randint(0, 100000)) for _ in keys[1:]]
     print("%s\t%s" % (xaxes[i], "\t".join(values)))
 
