060a2a6c0cda8f0eaba3d08acc602dca7fe173d3
SeongJae Park <sj38.park@gmail.com>
2021-10-03 10:31:28 +0200

    gnuplot/scatter_datgen: Add an option for max value
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- gnuplot/scatter_datgen.py
+++ gnuplot/scatter_datgen.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+import argparse
 import random
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--max', type=int, default=100,
+        help='max value in the data')
+args = parser.parse_args()
 
 keys = ["system_A", "systemB", "systemC"]
 xaxes = range(1,5)
@@ -8,6 +14,6 @@ xaxes = range(1,5)
 for idx, k in enumerate(keys):
     print(k)
     for x in xaxes:
-        print("%d %d" % (x, random.randint(0, 100)))
+        print("%d %d" % (x, random.randint(0, args.max)))
     if idx < len(keys) - 1:
         print("\n")
