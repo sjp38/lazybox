0f9ab2f7a29087d908663069c86077fd0b517a12
SeongJae Park <sj38.park@gmail.com>
2021-10-04 11:26:28 +0200

    gnuplot/example: Make the script quit whenever something wrong
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- gnuplot/example.sh
+++ gnuplot/example.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 ODIR="example_plots"
 mkdir -p $ODIR
 
@@ -10,14 +12,14 @@ OPTS+="--gnuplot_cmds"
 OPTS=($OPTS)
 
 ./scatter_datgen.py | ./plot.py $ODIR/scatter.pdf "${OPTS[@]}" \
-	--font 'times new roman,20' \
-&& \
+	--font 'times new roman,20'
+
 ./scatter-yerr_datgen.py | ./plot.py $ODIR/scatter-yerr.pdf \
-	--type scatter-yerr "${OPTS[@]}" --xlog --xtitle "examle x title" \
-&& \
+	--type scatter-yerr "${OPTS[@]}" --xlog --xtitle "examle x title"
+
 ./clustered_box_datgen.py | ./plot.py $ODIR/cluster.pdf \
-	--type clustered_boxes "${OPTS[@]}" --ytitle "example y title" \
-&& \
+	--type clustered_boxes "${OPTS[@]}" --ytitle "example y title"
+
 ./clustered_box-yerr_datgen.py | ./plot.py $ODIR/cluster-yerr.pdf \
 	--type clustered_boxes-yerr "${OPTS[@]}"
 
