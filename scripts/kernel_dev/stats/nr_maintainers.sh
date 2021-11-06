c60621b298018919210f76e8257720339ba72296
SeongJae Park <sj38.park@gmail.com>
2021-11-06 10:59:59 +0100

    nr_maintainers: Receive linux repo as argument
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/kernel_dev/stats/nr_maintainers.sh
+++ scripts/kernel_dev/stats/nr_maintainers.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <linux repo>"
+	exit 1
+fi
+
+linux_repo=$1
+
 for major in 2.6 3 4 5
 do
 	for minor in {1..20}
 	do
 		version="v$major.$minor"
-		nr_maintainers=$(git show "$version":MAINTAINERS 2> /dev/null | \
+		nr_maintainers=$(git -C "$linux_repo" \
+			show "$version":MAINTAINERS 2> /dev/null | \
 			grep '^M:' | sort | uniq -c | wc -l)
 		if [ "$nr_maintainers" = "0" ]
 		then
