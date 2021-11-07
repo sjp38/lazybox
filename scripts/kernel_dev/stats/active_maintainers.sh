a990fed3f0ef5a068906acb1eff8936fa74246ea
SeongJae Park <sj38.park@gmail.com>
2021-11-07 13:29:38 +0100

    kernel_dev/stat/active_maintainers: Allow users set activeness threshold
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/kernel_dev/stats/active_maintainers.sh
+++ scripts/kernel_dev/stats/active_maintainers.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
-	echo "Usage: $0 <linux repo>"
+	echo "Usage: $0 <linux repo> [activeness threshold in days]"
 	exit 1
 fi
 
 linux_repo=$1
 
+if [ $# -ge 2 ]
+then
+	active_thres_days=$2
+else
+	active_thres_days=180
+fi
+
 maintainers=$(git -C "$linux_repo" show "$version":MAINTAINERS 2> /dev/null | \
 	grep '^M:' | sort | uniq | awk -F'^M:\t' '{print $2}')
 nr_maintainers=$(echo "$maintainers" | wc -l)
 
-since_date=$(date --date='-6 month')
+since_date=$(date --date="-$active_thres_days day")
 nr_active_maintainers=0
 while IFS= read -r author
 do
