1b75c23328b9a9ea1e0202f0e4a2efd75aaa8725
SeongJae Park <sj38.park@gmail.com>
2021-12-19 09:58:24 +0100

    git_stats: Add a script for listing authors
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ scripts/git_stats/authors.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('repo', help='git repositories to get the stat from')
+    args = parser.parse_args()
+
+    cmd = ('git -C %s log' % args.repo).split()
+    cmd.append('--pretty="%an <%ae>"')
+    authors_lines = subprocess.check_output(cmd).decode().strip().split('\n')
+    authors = {}
+    for author in authors_lines:
+        if not author in authors:
+            authors[author] = 0
+        authors[author] += 1
+
+    for author in authors:
+        print('%s: %d' % (author, authors[author]))
+
+if __name__ == '__main__':
+    main()
