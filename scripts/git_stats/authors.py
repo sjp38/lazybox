ac26b5964807f0817510e3cb4817040bc8bbebf9
SeongJae Park <sj38.park@gmail.com>
2021-12-19 10:03:08 +0100

    git_stats/authors: Implement '--since' and '--until' options
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/git_stats/authors.py
+++ scripts/git_stats/authors.py
@@ -6,10 +6,16 @@ import subprocess
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('repo', help='git repositories to get the stat from')
+    parser.add_argument('--since', help='since when in YYYY-MM-DD format')
+    parser.add_argument('--until', help='until when in YYYY-MM-DD format')
     args = parser.parse_args()
 
     cmd = ('git -C %s log' % args.repo).split()
     cmd.append('--pretty="%an <%ae>"')
+    if args.since:
+        cmd.append('--since=%s' % args.since)
+    if args.until:
+        cmd.append('--until=%s' % args.until)
     authors_lines = subprocess.check_output(cmd).decode().strip().split('\n')
     authors = {}
     for author in authors_lines:
