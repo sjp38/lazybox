7bf314751d33244d4abda2e24b69f2b60cde5219
SeongJae Park <sj38.park@gmail.com>
2022-01-06 07:09:27 +0100

    gnuplot/show_term_colors: Align texts
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- gnuplot/show_term_colors.py
+++ gnuplot/show_term_colors.py
@@ -4,5 +4,5 @@ for i in range(0, 16):
     line = ''
     for j in range(0, 16):
         val = i * 16 + j
-        line += u'\u001b[48;5;%dm %d \u001b[0m' % (val, val)
+        line += u'\u001b[48;5;%dm %03d \u001b[0m' % (val, val)
     print(line)
