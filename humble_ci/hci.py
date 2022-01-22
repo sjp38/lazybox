14f56c97c7da5c7ffa189c032bfd5ccac7ddb216
SeongJae Park <sj38.park@gmail.com>
2022-01-22 09:49:47 +0100

    Add a python program for humble continuous integration
    This commit adds a python program for very simple continuous integration
    tests.  It's actually just a hello world program containing the TODO
    list at the moment.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ humble_ci/hci.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+Assumption
+- This script is periodically called by cron
+Input
+- path to the repo
+- trees to check, and
+- test to run against those
+Works
+- check if the trees have updated
+- run the test against the updated ones, and
+- provide the reports
+'''
+
+def main():
+    print('hello')
+
+if __name__ == '__main__':
+    main()
