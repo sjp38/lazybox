d86df64f40543c8473b9f10c7e5475f42e8abfd1
SeongJae Park <sj38.park@gmail.com>
2022-01-22 10:30:10 +0100

    humble_ci: Implement initial setup
    This commit implements the initial setup part of humble_ci.  If the
    given repo is not exist, clone the repo and add remotes.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- humble_ci/hci.py
+++ humble_ci/hci.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+import subprocess
 
 '''
 Assumption
@@ -25,7 +27,35 @@ def main():
     parser.add_argument('--test', metavar='<path>',
             help='test to run')
     args = parser.parse_args()
-    print(args)
+
+    if not args.repo or not args.tree_to_track or not args.test:
+        print('all options should be given')
+        exit(1)
+
+    if not os.path.isdir(args.repo):
+        name, url, branch = args.tree_to_track[0]
+        # git clone --origin $name $url $args.repo
+        cmd = 'git clone --origin'.split()
+        cmd += [name, url, args.repo]
+        try:
+            subprocess.check_output(cmd)
+        except subprocess.CalledProcessError as e:
+            print('cloning the repo (\'%s\') failed' % ' '.join(cmd))
+            exit(1)
+        for name, url, branch in args.tree_to_track[1:]:
+            cmd = ['git', '-C', args.repo, 'remote', 'add']
+            cmd += [name, url]
+            try:
+                subprocess.check_output(cmd)
+            except subprocess.CalledProcessError as e:
+                print('adding retmote (\'%s\') failed' % ' '.join(cmd))
+                exit(1)
+        cmd = ['git', '-C', args.repo, 'remote', 'update']
+        try:
+            subprocess.check_output(cmd)
+        except subprocess.CalledProcessError as e:
+            print('updating remotes (\'%s\') failed' % ' '.join(cmd))
+            exit(1)
 
 if __name__ == '__main__':
     main()
