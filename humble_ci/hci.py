0a4e912eb2df943e3626e3d44eebf162fcbc0b1f
SeongJae Park <sj38.park@gmail.com>
2022-01-30 13:28:37 +0100

    humble_ci: Implement update checking as a class method
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- humble_ci/hci.py
+++ humble_ci/hci.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import subprocess
 import time
+import json
 
 '''
 Checks update to the given program and run the given test if there was any
@@ -65,6 +66,69 @@ class HciTest:
                 raise ValueError('skip reason is not given')
             self.skip_reason = skip_reason
 
+    def git_remote_added(self):
+        try:
+            remotes = subprocess.check_output(
+                    ['git', '-C', self.repo, 'remote']).decode().strip().split()
+            if not self.tree[0] in remotes:
+                return False
+        except subprocess.CalledProcessError as e:
+            print('git remote failed')
+            self.set_state('finished', 'skip', 'git remote check failed')
+            return False
+        return True
+
+    def check_update(self):
+        if self.state != 'check_update':
+            return
+
+        name, url, branch = self.tree
+        git_cmd = ['git', '-C', self.repo]
+        if not os.path.isdir(os.path.join(self.repo, '.git')):
+            os.mkdir(self.repo)
+            try:
+                subprocess.check_output(git_cmd + ['init'])
+            except subprocess.CalledProcessError as e:
+                print('git init failed')
+                self.set_state('finished', 'skip', 'git init failed')
+
+        tracking = self.git_remote_added()
+        if self.state == 'finished':
+            return
+
+        if not tracking:
+            try:
+                subprocess.check_output(git_cmd + ['remote', 'add', name, url])
+                self.past_commit = None
+            except subprocess.CalledProcessError as e:
+                print('adding remote (\'%s\') failed' % self.tree_git_ref())
+                self.set_state('finished', 'skip', 'adding remote failed')
+        else:
+            try:
+                cmd = git_cmd + ['rev-parse', self.tree_git_ref()]
+                commit = subprocess.check_output(cmd).decode().strip()
+                self.past_commit = commit
+            except subprocess.CalledProcessError as e:
+                print('getting hash of %s failed' % self.tree_git_ref())
+                self.set_state('finished', 'skip', 'getting past hash failed')
+
+        if self.state == 'finished':
+            return
+
+        try:
+            subprocess.check_output(git_cmd + ['fetch', name])
+        except subprocess.CalledProcessError as e:
+            print('fetching %s failed' % self.tree_git_ref())
+            self.set_state('finished', 'skip', 'fetching failed')
+
+        try:
+            cmd = git_cmd + ['rev-parse', self.tree_git_ref()]
+            commit = subprocess.check_output(cmd).decode().strip()
+            self.current_commit = commit
+        except subprocess.CalledProcessError as e:
+            print('getting hash of %s failed' % self.tree_git_ref())
+            self.set_state('finished', 'skip', 'getting current hash failed')
+
 def run_tests(tests):
     for test in tests:
         if test.state == 'finished':
@@ -103,54 +167,6 @@ def run_tests(tests):
                 print('# FAIL %s' % ref_hash)
                 test.set_state('finished', 'fail')
 
-def git_remote_update(repo):
-    cmd = ['git', '-C', repo, 'remote', 'update']
-    try:
-        subprocess.check_output(cmd)
-    except subprocess.CalledProcessError as e:
-        print('updating remotes (\'%s\') failed' % ' '.join(cmd))
-        exit(1)
-
-def set_repo(repo, trees_to_track):
-    name, url, branch = trees_to_track[0]
-    cmd = 'git clone --origin'.split()
-    cmd += [name, url, repo]
-    try:
-        subprocess.check_output(cmd)
-    except subprocess.CalledProcessError as e:
-        print('cloning the repo (\'%s\') failed' % ' '.join(cmd))
-        exit(1)
-    for name, url, branch in trees_to_track[1:]:
-        cmd = ['git', '-C', repo, 'remote', 'add']
-        cmd += [name, url]
-        try:
-            subprocess.check_output(cmd)
-        except subprocess.CalledProcessError as e:
-            print('adding retmote (\'%s\') failed' % ' '.join(cmd))
-            exit(1)
-    git_remote_update(repo)
-
-def get_refs_commits(repo, trees_to_track):
-    refs_commits = {}
-
-    if not os.path.isdir(repo):
-        set_repo(repo, trees_to_track)
-        for name, url, branch in trees_to_track:
-            refs_commits['%s/%s' % (name, branch)] = None
-        return refs_commits
-
-    for name, url, branch in trees_to_track:
-        ref_to_check = '%s/%s' % (name, branch)
-        cmd = ['git', '-C', repo, 'rev-parse', ref_to_check]
-        try:
-            commit = subprocess.check_output(cmd).decode().strip()
-        except subprocess.CalledProcessError as e:
-            print('getting hash of %s (\'%s\') failed' %
-                    (ref_to_check, ' '.join(cmd)))
-            exit(1)
-        refs_commits[ref_to_check] = commit
-    return refs_commits
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--repo', metavar='<path>',
@@ -186,17 +202,7 @@ def main():
         print('# get references before update')
         for test in tests:
             test.set_state('check_update')
-        before_update_commits = get_refs_commits(args.repo, args.tree_to_track)
-        for test in tests:
-            test.past_commit = before_update_commits[test.tree_git_ref()]
-
-        print('# update remotes')
-        git_remote_update(args.repo)
-
-        print('# get references after update')
-        after_update_commits = get_refs_commits(args.repo, args.tree_to_track)
-        for test in tests:
-            test.current_commit = after_update_commits[test.tree_git_ref()]
+            test.check_update()
 
         print('# schedule tests')
         for test in tests:
