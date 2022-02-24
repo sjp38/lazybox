d133e3f0175ac6989c33a393076768b80dace089
SeongJae Park <sj38.park@gmail.com>
2022-02-24 22:55:17 +0100

    fs/write: Support lists
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/fs.py
+++ scripts/fs.py
@@ -21,6 +21,11 @@ def read_fs(root, strip_content, max_depth, current_depth=1):
     return contents
 
 def write_fs(root, contents):
+    if isinstance(contents, list):
+        for c in contents:
+            write_fs(root, c)
+        return
+
     for filename in contents:
         filepath = os.path.join(root, filename)
         if os.path.isfile(filepath):
@@ -39,6 +44,8 @@ def main():
     parser.add_argument('--dont_strip_content', action='store_true',
             help='strip contents of files')
     parser.add_argument('--contents', help='contents to write')
+    parser.add_argument('--content_file',
+            help='file having the content to write')
     args = parser.parse_args()
 
     if args.command == 'read':
@@ -48,7 +55,12 @@ def main():
         print(json.dumps(read_fs(args.root, not args.dont_strip_content,
             args.max_depth), indent=4, sort_keys=True))
     elif args.command == 'write':
-        write_fs(args.root, json.loads(args.contents))
+        if args.contents != None:
+            contents = args.contents
+        elif args.content_file:
+            with open(args.content_file, 'r') as f:
+                contents = f.read()
+        write_fs(args.root, json.loads(contents))
 
 if __name__ == '__main__':
     main()
