0fc10e33df8041592988d75db232ecea288c9b17
SeongJae Park <sj38.park@gmail.com>
2022-03-20 10:30:20 +0100

    kernel_dev/rm_kernels: Add options for specifying kernels to leave
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/kernel_dev/rm_kernels.sh
+++ scripts/kernel_dev/rm_kernels.sh
@@ -2,7 +2,11 @@
 
 pr_usage()
 {
-	echo "Usage: $0 <target kernel version>..."
+	echo "Usage: $0 [OPTION]... [target kernel version]..."
+	echo
+	echo "OPTION"
+	echo "  --except_old <number>	Leave <number> oldest kernels"
+	echo "  --except_new <number>	Leave <number> latest kernels"
 }
 
 pr_usage_exit()
@@ -18,13 +22,69 @@ then
 	pr_usage_exit 1
 fi
 
+kernels_to_remove=()
+except_old_nr=0
+except_new_nr=0
+
+while [ $# -ne 0 ]
+do
+	case $1 in
+	"--except_old")
+		if [ $# -lt 2 ]
+		then
+			echo "<number> not given"
+			pr_usage_exit 1
+		fi
+		except_old_nr=$2
+		shift 2
+		continue
+		;;
+	"--except_new")
+		if [ $# -lt 2 ]
+		then
+			echo "<number> not given"
+			pr_usage_exit 1
+		fi
+		except_new_nr=$2
+		shift 2
+		continue
+		;;
+	*)
+		if [ "$except_old_nr" = "" ] && [ "$except_new_nr" = "" ] &&
+			[ $# -lt 1 ]
+		then
+			echo "<target kernel version> not given"
+			pr_usage_exit 1
+		fi
+		kernels_to_remove=($@)
+		break
+		;;
+	esac
+done
+
+bindir=$(dirname "$0")
+read -r -a kernels <<< "$("$bindir/ls_kernels.py")"
+
+rm_start=$except_old_nr
+rm_end=$((${#kernels[@]} - except_new_nr))
+
+for ((i = 0 ; i < ${#kernels[@]} ; i++))
+do
+	echo "$i"
+	if [ $i -lt $rm_start ] || [ $i -ge $rm_end ]
+	then
+		continue
+	fi
+	kernels_to_remove+=("${kernels[$i]}")
+done
+
 if [ "$EUID" -ne 0 ]
 then
 	echo "run as root, please"
 	exit 1
 fi
 
-for ver in "${@:1}"
+for ver in "${kernels_to_remove[@]}"
 do
 	if [ ! -e "/boot/vmlinuz-$ver" ]
 	then
