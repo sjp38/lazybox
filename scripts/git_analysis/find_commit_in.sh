4e294df2250ce3925a76d12767b14e3626d3acad
SeongJae Park <sj38.park@gmail.com>
2022-09-30 09:11:26 -0700

    git_analysis: Add a script for finding merged commit
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ scripts/git_analysis/find_commit_in.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+	echo "Usage: $0 <commit> <commit range>"
+	echo
+	echo "	Find a commit in <commit range> that has the author name and"
+	echo "	subject of <commit>"
+	exit 1
+fi
+
+commit_to_find=$1
+commit_range=$2
+
+author=$(git log -n 1 "$commit_to_find" --pretty=%an)
+subject=$(git log -n 1 "$commit_to_find" --pretty=%s)
+
+git log --author="$author" --oneline | grep -i -m 1 "$subject"
