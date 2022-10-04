c501ed792b4f06b1f0a3abfb61d763740bea6734
SeongJae Park <sj38.park@gmail.com>
2022-10-04 07:12:14 -0700

    git_analysis/find_commit_in: Find commit in the given commit range
    find_commit_in.sh is currently searching the commit in the checked-out
    tree instead of the given commit range.  Fix the problem by searching
    the commit in the given commit range.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- scripts/git_analysis/find_commit_in.sh
+++ scripts/git_analysis/find_commit_in.sh
@@ -15,4 +15,4 @@ commit_range=$2
 author=$(git log -n 1 "$commit_to_find" --pretty=%an)
 subject=$(git log -n 1 "$commit_to_find" --pretty=%s)
 
-git log --author="$author" --oneline | grep -i -m 1 "$subject"
+git log --author="$author" --oneline "$commit_range" | grep -i -m 1 "$subject"
