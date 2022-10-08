c17f4814cb1fd1c606fbb3129035624a690423d4
SeongJae Park <sj38.park@gmail.com>
2022-10-08 18:38:53 +0000

    git_helpers/find_commit_in: Support hash only printing
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- git_helpers/find_commit_in.sh
+++ git_helpers/find_commit_in.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
-then
-	echo "Usage: $0 <commit> <commit range>"
+pr_usage()
+{
+	echo "Usage: $0 [OPTION]... <commit> <commit range>"
 	echo
 	echo "	Find a commit in <commit range> that has the author name and"
 	echo "	subject of <commit>"
 	exit 1
+}
+
+if [ $# -lt 2 ]
+then
+	pr_usage
 fi
+
+hash_only="false"
+while [ $# -ne 0 ]
+do
+	case $1 in
+	"--hash_only")
+		hash_only="true"
+		shift 1
+		continue
+		;;
+	*)
+		if [ $# -ne 2 ]
+		then
+			pr_usage
+		fi
+		break
+		;;
+	esac
+done
 
 commit_to_find=$1
 commit_range=$2
@@ -15,4 +39,13 @@ commit_range=$2
 author=$(git log -n 1 "$commit_to_find" --pretty=%an)
 subject=$(git log -n 1 "$commit_to_find" --pretty=%s)
 
-git log --author="$author" --oneline "$commit_range" | grep -i -m 1 "$subject"
+hash_subject=$(git log --author="$author" --oneline "$commit_range" | \
+	grep -i -m 1 "$subject")
+if [ "$hash_only" = "true" ]
+then
+	simple_hash=$(echo "$hash_subject" | awk '{print $1}')
+	git log --pretty=%H -n 1 "$simple_hash"
+	exit
+fi
+
+echo "$hash_subject"
