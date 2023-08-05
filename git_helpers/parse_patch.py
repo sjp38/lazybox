540583e7934db3c96876b2979d38c00fa09ee711
SeongJae Park <sj38.park@gmail.com>
2023-08-05 14:31:51 -0700

    git_helpers: Add a script for simple patch parsing
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ git_helpers/parse_patch.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+
+import _patch
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('patch', metavar='<file>',
+            help='patch file to read')
+    parser.add_argument('fields', choices=['subject', 'date', 'author',
+        'mail_header', 'description', 'diff'],
+            nargs='+',
+            help='fields to read from the patch')
+    args = parser.parse_args()
+
+    patch = _patch.Patch(args.patch)
+    for field in args.fields:
+        if field == 'subject':
+            print(patch.subject)
+        elif field == 'date':
+            print(patch.date)
+        elif field == 'author':
+            print(patch.author)
+        elif field == 'mail_header':
+            print(patch.email_header)
+        elif field == 'description':
+            print(patch.description_body)
+        elif field == 'diff':
+            print(patch.diff)
+
+if __name__ == '__main__':
+    main()
