62cf8bc793656dbe4f0c14909c92217073bea8e7
SeongJae Park <sj38.park@gmail.com>
2023-08-05 15:12:25 -0700

    git_helpers/patch_find: Support finding matching patch from patch files
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- git_helpers/patch_find.py
+++ git_helpers/patch_find.py
@@ -8,16 +8,31 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('patch', metavar='<file>',
             help='path to the patch file')
-    parser.add_argument('commits', metavar='<commits range reference>',
+    parser.add_argument('--commits', metavar='<commits range reference>',
             help='commits range to find patch in')
+    parser.add_argument('--patches', metavar='<patch file>', nargs='+',
+            help='patch files to find patch in')
     args = parser.parse_args()
 
+    if args.commits == None and args.patches == None:
+        print('--commits or --patches should be given')
+        parser.print_help()
+        exit(1)
+
     patch = _patch.Patch(args.patch)
-    try:
-        print('%s ("%s")' %
-                (patch.commit_in(args.commits)[:12], patch.subject))
-    except:
-        # not found
+    if args.commits:
+        try:
+            print('%s ("%s")' %
+                    (patch.commit_in(args.commits)[:12], patch.subject))
+        except:
+            # not found
+            exit(1)
+    else:
+        for patch_file in args.patches:
+            if patch == _patch.Patch(patch_file):
+                print(patch_file)
+                exit(0)
+        print('no matching patch file found')
         exit(1)
 
 if __name__ == '__main__':
