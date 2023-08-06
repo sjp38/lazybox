9dc4ac09c8f194f7fcb31692fb7c5356c50de0e6
SeongJae Park <sj38.park@gmail.com>
2023-08-06 12:51:05 -0700

    git_helpers/patch_parse: Use _git instead
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- git_helpers/patch_parse.py
+++ git_helpers/patch_parse.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-import _patch
+import _git
 
 def main():
     parser = argparse.ArgumentParser()
@@ -14,22 +14,22 @@ def main():
             help='fields to read from the patch')
     args = parser.parse_args()
 
-    patch = _patch.Patch(args.patch)
+    patch = _git.Patch(args.patch, set_diff=True)
     for field in args.fields:
         if field == 'subject':
-            print(patch.subject)
+            print(patch.change.subject)
         elif field == 'date':
-            print(patch.date)
+            print(patch.sent_date)
         elif field == 'author':
-            print(patch.author)
+            print(patch.change.author)
         elif field == 'mail_header':
             print(patch.email_header)
         elif field == 'description':
-            print(patch.description_body)
+            print(patch.change.description)
         elif field == 'diff':
-            print(patch.diff)
+            print(patch.change.diff)
         elif field == 'fixes':
-            print('\n'.join(patch.fixes))
+            print('\n'.join(patch.change.get_fixing_commit_refs()))
 
 if __name__ == '__main__':
     main()
