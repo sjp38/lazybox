16ad8cacc79d7a2283034ae5e4d5a06746773881
SeongJae Park <sj38.park@gmail.com>
2023-08-12 14:14:44 -0700

    git_helpers/fixes: Make error message comment format
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- git_helpers/fixes.py
+++ git_helpers/fixes.py
@@ -38,7 +38,7 @@ def main():
             try:
                 buggy_change = _git.Change(commit=hashid, repo=args.repo)
             except:
-                print('Failed parsing %s' % bug_reference)
+                print('# Failed parsing %s' % bug_reference)
                 continue
             for patch_or_commits_range in args.bugs:
                 if os.path.isfile(patch_or_commits_range):
