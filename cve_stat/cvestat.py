23235a4bb4d53a9a1a1c5949952df48ca37d8850
sjpark <sj38.park@gmail.com>
2023-08-20 11:19:48 -0700

    cve_stat/cvestat.py: Split out threshold based per-tree stat function
    Signed-off-by: sjpark <sj38.park@gmail.com>

--- cve_stat/cvestat.py
+++ cve_stat/cvestat.py
@@ -4,6 +4,27 @@ import argparse
 
 import _linux_kernel_cve
 
+def pr_fix_to_report_stat(cves, tree, thresholds):
+    nrs_for_thres = {thres: 0 for thres in thresholds}
+    nr_total = 0
+    for cve in cves:
+        if not tree in cve.break_commits:
+            continue
+        nr_total += 1
+        if not tree in cve.fix_commits:
+            continue
+        committed_date = cve.fix_commits[tree].committed_date
+
+        for thres in thresholds:
+            if committed_date <= cve.added_date - thres:
+                nrs_for_thres[thres] += 1
+    print('%s tree' % tree)
+    for thres in thresholds:
+        print('%d/%d (%.3f %%) CVEs are fixed %d weeks before being added' %
+                (nrs_for_thres[thres], nr_total,
+                    nrs_for_thres[thres] / nr_total * 100,
+                    thres / 7 / 24 / 3600))
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('dumpfile',
