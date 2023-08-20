5a0e102832a326d80e16079e69d43af3f20bbd0f
sjpark <sj38.park@gmail.com>
2023-08-20 11:27:52 -0700

    cve_stat/cvestat: Use pr_fix_to_report_stat()
    Signed-off-by: sjpark <sj38.park@gmail.com>

--- cve_stat/cvestat.py
+++ cve_stat/cvestat.py
@@ -37,35 +37,18 @@ def main():
     cves = _linux_kernel_cve.load_kernel_cves_from_json(args.dumpfile).values()
     for tree in ['mainline', '6.4', '6.1', '5.15', '5.10', '5.4', '4.19',
             '4.14']:
-        nr_cves = 0
-        nr_fixed_before_added = {
-                1: 0,
-                7 * 24 * 3600: 0,
-                2 * 7 * 24 * 3600: 0,
-                4 * 7 * 24 * 3600: 0,
-                8 * 7 * 24 * 3600: 0,
-                16 * 7 * 24 * 3600: 0,
-                32 * 7 * 24 * 3600: 0,
-                64 * 7 * 24 * 3600: 0,
-                128 * 7 * 24 * 3600: 0,
-                }
-        for cve in cves:
-            if not tree in cve.break_commits:
-                continue
-            nr_cves += 1
-            if not tree in cve.fix_commits:
-                continue
-            committed_date = cve.fix_commits[tree].committed_date
-
-            for thres in nr_fixed_before_added:
-                if committed_date <= cve.added_date - thres:
-                    nr_fixed_before_added[thres] += 1
-        print('%s tree' % tree)
-        for thres in sorted(nr_fixed_before_added.keys()):
-            print('%d/%d (%.3f %%) CVEs are fixed %d weeks before being added' %
-                    (nr_fixed_before_added[thres], nr_cves,
-                        nr_fixed_before_added[thres] / nr_cves * 100,
-                        thres / 7 / 24 / 3600))
+        pr_fix_to_report_stat(cves, tree,
+                [
+                    1,
+                    7 * 24 * 3600,
+                    2 * 7 * 24 * 3600,
+                    4 * 7 * 24 * 3600,
+                    8 * 7 * 24 * 3600,
+                    16 * 7 * 24 * 3600,
+                    32 * 7 * 24 * 3600,
+                    64 * 7 * 24 * 3600,
+                    128 * 7 * 24 * 3600,
+                    ], before=True)
         print()
 
 if __name__ == '__main__':
