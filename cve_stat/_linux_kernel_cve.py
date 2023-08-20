0691aa4174da588d475e489e6099cd140f8114bf
sjpark <sj38.park@gmail.com>
2023-08-19 19:13:37 -0700

    cve_stat/_linux_kernel_cve: Implement LinuxKernelCve constructor
    Signed-off-by: sjpark <sj38.park@gmail.com>

--- cve_stat/_linux_kernel_cve.py
+++ cve_stat/_linux_kernel_cve.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import json
+import os
 import subprocess
 
 class CommitInfo:
@@ -25,3 +27,72 @@ class LinuxKernelCve:
     fix_commits = None      # {kernel tree: CommitInfo}
     cvss_scores = None      # {CVSS version: score}
     added_date = None       # unix timestamp
+
+    def __init__(self, name, linux_kernel_cves_repo,
+            main_infos, stream_breaks, stream_fixes):
+        # linux_kernel_cves_repo: path to linux_kernel_cves local repo
+        # main_infos: linux_kernel_cves/data/kernel_cves.json parsed dict
+        # stream_breaks: linux_kernel_cves/data/stream_data.json parsed dict
+        # stream_fixes: linux_kernel_cves/data/stream_fixes.json parsed dict
+
+        data_dir=os.path.join(linux_kernel_cves_repo, 'data')
+        if main_infos == None:
+            with open(os.path.join(data_dir, 'kernel_cves.json'), 'r') as f:
+                main_infos = json.load(f)
+        if stream_breaks == None:
+            with open(os.path.join(data_dir, 'stream_data.json'), 'r') as f:
+                stream_breaks = json.load(f)
+        if stream_fixes == None:
+            with open(os.path.join(data_dir, 'stream_fixes.json'), 'r') as f:
+                stream_fixes = json.load(f)
+
+        self.name = name
+
+        if not name in main_infos:
+            return
+        main_info = main_infos[name]
+
+        self.break_commits = {}
+        if 'breaks' in main_info:
+            self.break_commits['mainline'] = main_info['breaks']
+            for stable_series, series_breaks in stream_breaks.items():
+                for stable_release, breaks in series_breaks.items():
+                    if not name in breaks:
+                        continue
+                    break_info = breaks[name]
+                    if not 'cmt_id' in break_info:
+                        continue
+                    self.break_commits[stable_series] = break_info['cmt_id']
+
+        self.fix_commits = {}
+        if 'fixes' in main_info:
+            self.fix_commits['mainline'] = main_info['fixes']
+            for stable_series, series_fixes in stream_fixes.items():
+                for stable_release, fixes in series_fixes.items():
+                    if not name in fixes:
+                        continue
+                    fix_info = fixes[name]
+                    if not 'cmt_id' in fix_info:
+                        continue
+                    self.fix_commits[stable_series] = fix_info['cmt_id']
+
+        self.cvss_scores = {}
+        if 'cvss1' in main_info and 'score' in main_info['cvss1']:
+            self.cvss_scores['cvss1'] = main_info['cvss1']['score']
+        if 'cvss2' in main_info and 'score' in main_info['cvss2']:
+            self.cvss_scores['cvss2'] = main_info['cvss2']['score']
+        if 'cvss3' in main_info and 'score' in main_info['cvss3']:
+            self.cvss_scores['cvss3'] = main_info['cvss3']['score']
+
+        data_file = os.path.join(data_dir, 'kernel_cves.json')
+        cve_line_number = int(subprocess.check_output([
+            'grep', '-n', '--max-count=1', '    "%s":' % name,
+            data_file]).decode().split(':')[0])
+        touch_commit_id = subprocess.check_output([
+            'git', '-C', linux_kernel_cves_repo, 'blame',
+            os.path.join('data', 'kernel_cves.json'),
+            '-L', '%d,%d' % (cve_line_number,
+                cve_line_number)]).decode().split()[0]
+        self.added_date = int(subprocess.check_output([
+            'git', '-C', linux_kernel_cves_repo, 'log', '-1', '--date=unix',
+            '--pretty=%ad', touch_commit_id]).decode().strip())
