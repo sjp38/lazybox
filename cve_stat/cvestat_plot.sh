679f78d0d24223bb888a8232f57fb0010c81b00b
sjpark <sj38.park@gmail.com>
2023-08-20 21:07:15 -0700

    cve_stat/cvestat_plot: Increase point size and make y-axis title clear
    Signed-off-by: sjpark <sj38.park@gmail.com>

--- cve_stat/cvestat_plot.sh
+++ cve_stat/cvestat_plot.sh
@@ -13,7 +13,7 @@ bindir=$(dirname "$0")
 "$bindir/cvestat_percentile.py" "$cve_json" \
 	--metric broken_to_reported | \
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
-	--pointsize 0.5 --xtitle "Percentile" \
+	--pointsize 1 --xtitle "Percentile" \
 	--ytitle "Days from a CVE is committed to be reported" \
 	--size 1024,768 \
 	--font "TimesNewRoman" "broken_to_reported_linear.png"
@@ -21,15 +21,15 @@ bindir=$(dirname "$0")
 "$bindir/cvestat_percentile.py" "$cve_json" \
 	--metric broken_to_reported --min_val 1 | \
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
-	--pointsize 0.5 --xtitle "Percentile" \
-	--ytitle "Days from a CVE is committed to be reported" \
+	--pointsize 1 --xtitle "Percentile" \
+	--ytitle "Days from a CVE is committed to be reported, excluding <1 (logscale)" \
 	--size 1024,768 \
 	--font "TimesNewRoman" --ylog "broken_to_reported.png"
 
 "$bindir/cvestat_percentile.py" "$cve_json" \
 	--metric report_to_fix_authored | \
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
-	--pointsize 0.5 --xtitle "Percentile" \
+	--pointsize 1 --xtitle "Percentile" \
 	--ytitle "Days from a CVE be reported to the fix be authored" \
 	--size 1024,768 \
 	--font "TimesNewRoman" "report_to_fix_authored_linear.png"
@@ -37,7 +37,7 @@ bindir=$(dirname "$0")
 "$bindir/cvestat_percentile.py" "$cve_json" \
 	--metric report_to_fix_committed | \
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
-	--pointsize 0.5 --xtitle "Percentile" \
+	--pointsize 1 --xtitle "Percentile" \
 	--ytitle "Days from a CVE be reported to the fix be committed" \
 	--size 1024,768 \
 	--font "TimesNewRoman" "report_to_fix_committed_linear.png"
@@ -45,15 +45,15 @@ bindir=$(dirname "$0")
 "$bindir/cvestat_percentile.py" "$cve_json" \
 	--metric report_to_fix_authored --min_val 1 | \
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
-	--pointsize 0.5 --xtitle "Percentile" \
-	--ytitle "Days from a CVE be reported to the fix be authored" \
+	--pointsize 1 --xtitle "Percentile" \
+	--ytitle "Days from a CVE be reported to the fix be authored, excluding <1 (logscale)" \
 	--size 1024,768 \
 	--font "TimesNewRoman" --ylog "report_to_fix_authored.png"
 
 "$bindir/cvestat_percentile.py" "$cve_json" \
 	--metric report_to_fix_committed --min_val 1 | \
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
-	--pointsize 0.5 --xtitle "Percentile" \
-	--ytitle "Days from a CVE be reported to the fix be committed" \
+	--pointsize 1 --xtitle "Percentile" \
+	--ytitle "Days from a CVE be reported to the fix be committed, exlcuding <1 (logscale)" \
 	--size 1024,768 \
 	--font "TimesNewRoman" --ylog "report_to_fix_committed.png"
