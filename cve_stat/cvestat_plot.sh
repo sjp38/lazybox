c9a43fda1dbac953c7208dc6096c7522fcc5ab6c
sjpark <sj38.park@gmail.com>
2023-08-20 21:00:53 -0700

    cve_stat/cvestat_plot.sh: Plot to png files
    Signed-off-by: sjpark <sj38.park@gmail.com>

--- cve_stat/cvestat_plot.sh
+++ cve_stat/cvestat_plot.sh
@@ -15,39 +15,45 @@ bindir=$(dirname "$0")
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
 	--pointsize 0.5 --xtitle "Percentile" \
 	--ytitle "Days from a CVE is committed to be reported" \
-	--font "TimesNewRoman, 5pt" "broken_to_reported_linear.pdf"
+	--size 1024,768 \
+	--font "TimesNewRoman" "broken_to_reported_linear.png"
 
 "$bindir/cvestat_percentile.py" "$cve_json" \
 	--metric broken_to_reported --min_val 1 | \
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
 	--pointsize 0.5 --xtitle "Percentile" \
 	--ytitle "Days from a CVE is committed to be reported" \
-	--font "TimesNewRoman, 5pt" --ylog "broken_to_reported.pdf"
+	--size 1024,768 \
+	--font "TimesNewRoman" --ylog "broken_to_reported.png"
 
 "$bindir/cvestat_percentile.py" "$cve_json" \
 	--metric report_to_fix_authored | \
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
 	--pointsize 0.5 --xtitle "Percentile" \
 	--ytitle "Days from a CVE be reported to the fix be authored" \
-	--font "TimesNewRoman, 5pt" "report_to_fix_authored_linear.pdf"
+	--size 1024,768 \
+	--font "TimesNewRoman" "report_to_fix_authored_linear.png"
 
 "$bindir/cvestat_percentile.py" "$cve_json" \
 	--metric report_to_fix_committed | \
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
 	--pointsize 0.5 --xtitle "Percentile" \
 	--ytitle "Days from a CVE be reported to the fix be committed" \
-	--font "TimesNewRoman, 5pt" "report_to_fix_committed_linear.pdf"
+	--size 1024,768 \
+	--font "TimesNewRoman" "report_to_fix_committed_linear.png"
 
 "$bindir/cvestat_percentile.py" "$cve_json" \
 	--metric report_to_fix_authored --min_val 1 | \
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
 	--pointsize 0.5 --xtitle "Percentile" \
 	--ytitle "Days from a CVE be reported to the fix be authored" \
-	--font "TimesNewRoman, 5pt" --ylog "report_to_fix_authored.pdf"
+	--size 1024,768 \
+	--font "TimesNewRoman" --ylog "report_to_fix_authored.png"
 
 "$bindir/cvestat_percentile.py" "$cve_json" \
 	--metric report_to_fix_committed --min_val 1 | \
 	"$bindir/../gnuplot/plot.py" --data_fmt recs --type scatter \
 	--pointsize 0.5 --xtitle "Percentile" \
 	--ytitle "Days from a CVE be reported to the fix be committed" \
-	--font "TimesNewRoman, 5pt" --ylog "report_to_fix_committed.pdf"
+	--size 1024,768 \
+	--font "TimesNewRoman" --ylog "report_to_fix_committed.png"
