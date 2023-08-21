31e708577d82500256910d0ba3a58fdf4cca484e
sjpark <sj38.park@gmail.com>
2023-08-20 20:38:37 -0700

    cve_stat/cvestat: Print broken to reported metrics
    Signed-off-by: sjpark <sj38.park@gmail.com>

--- cve_stat/cvestat.py
+++ cve_stat/cvestat.py
@@ -4,6 +4,27 @@ import argparse
 
 import _linux_kernel_cve
 
+def pr_break_to_report_stat(cves, tree, thresholds):
+    nrs_for_thres = {thres: 0 for thres in thresholds}
+    nr_total = 0
+    for cve in cves:
+        if not tree in cve.break_commits:
+            continue
+        nr_total += 1
+        if not tree in cve.break_commits:
+            continue
+        committed_date = cve.break_commits[tree].committed_date
+
+        for thres in thresholds:
+            if committed_date >= cve.added_date + thres:
+                nrs_for_thres[thres] += 1
+    print('%s tree' % tree)
+    for thres in thresholds:
+        print('%d/%d (%.3f %%) CVEs are reported %d weeks after committed' %
+                (nrs_for_thres[thres], nr_total,
+                    nrs_for_thres[thres] / nr_total * 100,
+                    thres / 7 / 24 / 3600)),
+
 def pr_fix_to_report_stat(cves, tree, thresholds, before):
     nrs_for_thres = {thres: 0 for thres in thresholds}
     nr_total = 0
@@ -57,6 +78,22 @@ def main():
                         128 * 7 * 24 * 3600,
                         ], before)
             print()
+    for tree in ['mainline', '6.4', '6.1', '5.15', '5.10', '5.4', '4.19',
+            '4.14']:
+        pr_break_to_report_stat(cves, tree,
+                [
+                    -1 * 7 * 24 * 3600,
+                    1,
+                    7 * 24 * 3600,
+                    2 * 7 * 24 * 3600,
+                    4 * 7 * 24 * 3600,
+                    8 * 7 * 24 * 3600,
+                    16 * 7 * 24 * 3600,
+                    32 * 7 * 24 * 3600,
+                    64 * 7 * 24 * 3600,
+                    128 * 7 * 24 * 3600,
+                    ])
+        print()
 
 if __name__ == '__main__':
     main()
