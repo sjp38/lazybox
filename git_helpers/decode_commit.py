4fc68e1862588b5f407bad4601dde6d811e3404b
SeongJae Park <sj38.park@gmail.com>
2024-03-09 13:52:23 -0800

    git_helpers: Add a script for decoding commit hash id in text
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ git_helpers/decode_commit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+
+def is_hash(word):
+    if len(word) < 10:
+        return False
+    for c in word:
+        if c not in '0123456789abcdef':
+            return False
+    return True
+
+for line in sys.stdin:
+    print(line.strip())
+    for word in line.split():
+        if is_hash(word):
+            try:
+                decoded = subprocess.check_output(['git', 'describe', word])
+            except:
+                decoded = 'unknown'
+            print('# decoding commit %s: %s' % (word, decoded))
