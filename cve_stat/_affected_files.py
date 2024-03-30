60ef49dfbd92f8685a9cec81fcd3dd54be34375f
SeongJae Park <sj38.park@gmail.com>
2024-03-30 11:44:20 -0700

    cve_stat/_affected_files: Support multiple CVEs
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- cve_stat/_affected_files.py
+++ cve_stat/_affected_files.py
@@ -5,21 +5,22 @@ import sys
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('cve_mbox', metavar='<file>',
+    parser.add_argument('cve_mbox', metavar='<file>', nargs='+',
                         help='cve description mbox file')
     args = parser.parse_args()
 
-    if args.cve_mbox == 'stdin':
-        cve_description = sys.stdin.read()
-    else:
-        with open(args.cve_mbox, 'r') as f:
-            cve_description = f.read()
+    for mbox in args.cve_mbox:
+        if mbox == 'stdin':
+            cve_description = sys.stdin.read()
+        else:
+            with open(mbox, 'r') as f:
+                cve_description = f.read()
 
-    paragraphs = cve_description.split('\n\n')
-    for par in paragraphs:
-        lines = par.split('\n')
-        if lines[0] == 'The file(s) affected by this issue are:':
-            print('\n'.join(l.strip() for l in lines[1:]))
+        paragraphs = cve_description.split('\n\n')
+        for par in paragraphs:
+            lines = par.split('\n')
+            if lines[0] == 'The file(s) affected by this issue are:':
+                print('\n'.join(l.strip() for l in lines[1:]))
     
 if __name__ == '__main__':
     main()
