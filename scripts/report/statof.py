59aeede118561a5466add407ff05be6256589fff
SeongJae Park <sj@kernel.org>
2025-06-16 19:57:49 -0700

    scripts/report/statof: define and use main()
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- scripts/report/statof.py
+++ scripts/report/statof.py
@@ -50,51 +50,55 @@ def single_file_stat(target, path):
 def pr_stderr(msg):
     sys.stderr.write(msg + "\n")
 
-parser = argparse.ArgumentParser(description=program_decr,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument('stat', choices=['avg', 'min', 'max', 'stdev', 'median'],
-        help='type of stat you want')
-parser.add_argument('files', metavar='file', type=str, nargs='+',
-        help='paths to files')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description=program_decr,
+            formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('stat', choices=['avg', 'min', 'max', 'stdev', 'median'],
+            help='type of stat you want')
+    parser.add_argument('files', metavar='file', type=str, nargs='+',
+            help='paths to files')
+    args = parser.parse_args()
 
-target = vars(args)['stat']
-paths = vars(args)['files']
+    target = vars(args)['stat']
+    paths = vars(args)['files']
 
-if len(paths) == 0:
-    parser.print_help()
-    exit(1)
+    if len(paths) == 0:
+        parser.print_help()
+        exit(1)
 
-if len(paths) == 1:
-    single_file_stat(target, paths[0])
-    exit(0)
+    if len(paths) == 1:
+        single_file_stat(target, paths[0])
+        exit(0)
 
-datas = []
-for p in paths:
-    with open(p, 'r') as f:
-        datas.append(f.read().strip().split('\n'))
-        if len(datas[0]) != len(datas[-1]):
-            print("[Error] Number of lines are different for %s!" % p)
-            exit(1)
-
-for lidx in range(len(datas[0])):
-    lines = [data[lidx] for data in datas]
-    values = []
-    for fidx in range(len(lines[0].split())):
-        fields = [l.split()[fidx] for l in lines]
-        value = ""
-        try:
-            nrs = [float(f) for f in fields]
-            value = get_stat(target, nrs)
-            if target == 'stdev':
-                avg = sum(nrs) / len(nrs)
-                if value > avg / 10:
-                    pr_stderr("[WARNING] stdev %s, avg %s, %.2f stdev/avg!" % (
-                        value, avg, float(value) / avg))
-        except ValueError:
-            if not all(f == fields[0] for f in fields):
-                print("[Error] different text field! %s" % fields)
+    datas = []
+    for p in paths:
+        with open(p, 'r') as f:
+            datas.append(f.read().strip().split('\n'))
+            if len(datas[0]) != len(datas[-1]):
+                print("[Error] Number of lines are different for %s!" % p)
                 exit(1)
-            value = fields[0]
-        values.append(value)
-    print("\t".join([str(v) for v in values]))
+
+    for lidx in range(len(datas[0])):
+        lines = [data[lidx] for data in datas]
+        values = []
+        for fidx in range(len(lines[0].split())):
+            fields = [l.split()[fidx] for l in lines]
+            value = ""
+            try:
+                nrs = [float(f) for f in fields]
+                value = get_stat(target, nrs)
+                if target == 'stdev':
+                    avg = sum(nrs) / len(nrs)
+                    if value > avg / 10:
+                        pr_stderr("[WARNING] stdev %s, avg %s, %.2f stdev/avg!" % (
+                            value, avg, float(value) / avg))
+            except ValueError:
+                if not all(f == fields[0] for f in fields):
+                    print("[Error] different text field! %s" % fields)
+                    exit(1)
+                value = fields[0]
+            values.append(value)
+        print("\t".join([str(v) for v in values]))
+
+if __name__ == '__main__':
+    main()
