d7f9002948fb43332cb824a6807e044093ac7518
SeongJae Park <sj@kernel.org>
2025-10-26 14:05:59 -0700

    git_helpers/fmt_fixes_line: prints Cc: stable@ only if it is linux repo
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- git_helpers/fmt_fixes_line.sh
+++ git_helpers/fmt_fixes_line.sh
@@ -9,6 +9,12 @@ fi
 
 broken=$1
 commit_str=$(git log -1 --abbrev=12 --pretty='%h ("%s")' "$broken")
-version=$(git describe --contains --match "v*" "$broken")
 echo "Fixes: ${commit_str}"
+
+# if this is linux kernel repo, further prints Cc: stable@
+if ! grep stable@vger.ekrnel.org MAINTAINERS --quiet &> /dev/null
+then
+	exit 0
+fi
+version=$(git describe --contains --match "v*" "$broken")
 echo "Cc: <stable@vger.kernel.org> # ${version}"
