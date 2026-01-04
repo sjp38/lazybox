e495e582e8c9f8497f2e09b9a8715e8c48f9a794
SeongJae Park <sj@kernel.org>
2026-01-04 13:50:45 -0800

    git_helpers/timeline_of_trees: record a commit after divergence
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- git_helpers/timeline_of_trees.py
+++ git_helpers/timeline_of_trees.py
@@ -31,11 +31,13 @@ class Event:
     commit = None
     event_type = None   # first_commit, last_common_commit, last_commit
     trees_of_event = None
+    after_diverge_commit_to_trees = None
 
     def __init__(self, commit, event_type, trees_of_event):
         self.commit = commit
         self.event_type = event_type
         self.trees_of_event = trees_of_event
+        self.after_diverge_commit_to_trees = {}
 
 def git_commit_of(hashid):
     output = subprocess.check_output(
@@ -47,6 +49,19 @@ def git_commit_of(hashid):
     subject = output[26:]
     return Commit(commit_date, hashid, subject)
 
+def add_after_diverge_commit(event, tree):
+    common_commit = event.commit.hash
+    after_diverge_commit = subprocess.check_output(
+            'git log --pretty=%%H %s..%s | tail -1' % (
+                common_commit, tree), shell=True).decode().strip()
+    # it was the last commit?
+    if after_diverge_commit == '':
+        after_diverge_commit = common_commit
+    if not after_diverge_commit in event.after_diverge_commit_to_trees:
+        event.after_diverge_commit_to_trees[after_diverge_commit] = [tree]
+    else:
+        event.after_diverge_commit_to_trees[after_diverge_commit].append(tree)
+
 def add_event(events, commit_hash, event_type, tree):
     for event in events:
         if event.commit.hash != commit_hash:
@@ -55,9 +70,11 @@ def add_event(events, commit_hash, event_type, tree):
             continue
         if not tree in event.trees_of_event:
             event.trees_of_event.append(tree)
+            add_after_diverge_commit(event, tree)
         return
     commit = git_commit_of(commit_hash)
     events.append(Event(commit, event_type, [tree]))
+    add_after_diverge_commit(events[-1], tree)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -77,8 +94,12 @@ def main():
         for tree_b in trees:
             if tree_a == tree_b:
                 continue
-            diverge_commit = subprocess.check_output(
-                    ['git', 'merge-base', tree_a, tree_b]).decode().strip()
+            try:
+                diverge_commit = subprocess.check_output(
+                        ['git', 'merge-base', tree_a, tree_b]).decode().strip()
+            except:
+                # no common commit
+                continue
             add_event(events, diverge_commit, 'last_common_commit', tree_a)
             add_event(events, diverge_commit, 'last_common_commit', tree_b)
 
