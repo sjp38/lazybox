804cc19cbf505d3824728cbdffd7e295547adc05
SeongJae Park <sj@kernel.org>
2026-01-17 11:26:38 -0800

    git_helpers: add a script for patches queue status parsing
    It is common to use quilt patch series, like mm.  Monitoring the status
    and changes on the series is useful for tracking the development.  Add a
    script for doing that.  The script does nothing at the moment but only
    document what it wants to be.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ git_helpers/quilt_series_summary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+'''
+Summarize patches queue status, for given quilt series.
+- Shows list of patches and patch series.
+  - Shows comments for the patches on the series file.
+- Shows review status.
+- Diff the status.
+  - New, removed, modified patches.
+  - Updated review status.
+  - Any comments on series.
+'''
