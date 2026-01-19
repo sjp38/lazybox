d9e505cb2deca1100c52a646bb523ce75d600860
SeongJae Park <sj@kernel.org>
2026-01-19 11:17:10 -0800

    linux_hack/mm_series_patches: provide patch counts per branch
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- linux_hack/mm_series_patches.py
+++ linux_hack/mm_series_patches.py
@@ -14,7 +14,7 @@ def pr_patch_detail(patch_name, series_path):
     txt_file = os.path.join(
             txt_dir, '%s.txt' % patch_name[:-1 * len('.patch')])
     if not os.path.isfile(txt_file):
-        return
+        return False
     with open(txt_file, 'r') as f:
         txt = f.read()
     series_desc = None
@@ -33,22 +33,41 @@ def pr_patch_detail(patch_name, series_path):
             continue
         if fields[0] == 'Subject:':
             print(' '.join(fields[1:]))
+    return True
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('series', metavar='<file>', help='the series file')
     args = parser.parse_args()
 
+    nr_patches = {'total': 0}
+    branches = {}
     with open(args.series, 'r') as f:
         for line in f:
             fields = line.split()
             if fields[0] == '#BRANCH':
+                branch_name = fields[1]
+                branches[branch_name] = True
+                if not branch_name in nr_patches:
+                    nr_patches[branch_name] = 0
                 print(line.strip())
             if fields[0] == '#ENDBRANCH':
+                branches[fields[1]] = False
                 print(line.strip())
             if line.startswith('#'):
                 continue
-            pr_patch_detail(fields[0], args.series)
+            is_patch = pr_patch_detail(fields[0], args.series)
+            if is_patch is False:
+                continue
+            for branch, now_in_it in branches.items():
+                if now_in_it is False:
+                    continue
+                nr_patches[branch] += 1
+            nr_patches['total'] += 1
+
+    print()
+    for branch, nr in nr_patches.items():
+        print('%s: %d patches' % (branch, nr))
 
 if __name__ == '__main__':
     main()
