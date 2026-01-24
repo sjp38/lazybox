13cb11a2d88a35fbcb45cdd1e5ffa53737a11726
SeongJae Park <sj@kernel.org>
2026-01-24 13:09:06 -0800

    linux_hack/mm_series_diff: print new patches
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- linux_hack/mm_series_diff.py
+++ linux_hack/mm_series_diff.py
@@ -11,6 +11,50 @@ def patch_detail_of(subject, patch_details):
             return detail
     return None
 
+def pr_new_patch(detail, before_branches, last_detail):
+    pr_patch_series = False
+    if detail.patch_series is not None:
+        if last_detail is None:
+            pr_patch_series = True
+        elif last_detail.patch_series != detail.patch_series:
+            pr_patch_series = True
+    if pr_patch_series:
+        print('- Patch series "%s" # %s patches' %
+              (detail.patch_series, detail.sz_series))
+
+    comments = []
+    if before_branches != []:
+        comments.append('was in %s' % ', '.join(before_branches))
+
+    indent = ''
+    if detail.patch_series is not None:
+        comments.append('%s/%s series' % (detail.idx_series, detail.sz_series))
+        indent = '  '
+
+    fields = ['%s- %s' % (indent,detail.subject)]
+    if comments != []:
+        fields.append('# %s' % ', '.join(comments))
+    print(' '.join(fields))
+
+def pr_new_patches_in(branch, before_details, after_details):
+    last_pr = None
+    for after in after_details:
+        if not branch in after.branches:
+            continue
+        if after.branches[branch] is False:
+            continue
+        before = patch_detail_of(after.subject, before_details)
+        if before is None:
+            before_branches = []
+        else:
+            before_branches = [
+                    b for b in before.branches if before.branches[b] is True]
+        if branch in before_branches:
+            continue
+
+        pr_new_patch(after, before_branches, last_pr)
+        last_pr = after
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('before', metavar='<file>',
@@ -48,19 +92,10 @@ def main():
         print('%20s: %4d -> %4d (%4d)' % (
             branch, before_nr, after_nr, after_nr - before_nr))
 
-    print()
-    print('Patches newly added')
-    for after in after_patch_details:
-        if patch_detail_of(after.subject, before_patch_details) is not None:
-            continue
-        print('- %s' % after.subject)
-
-    print()
-    print('Patches dropped')
-    for before in before_patch_details:
-        if patch_detail_of(before.subject, after_patch_details) is not None:
-            continue
-        print('- %s' % before.subject)
+    for branch in branches:
+        print()
+        print('Patches added to %s' % branch)
+        pr_new_patches_in(branch, before_patch_details, after_patch_details)
 
 if __name__ == '__main__':
     main()
