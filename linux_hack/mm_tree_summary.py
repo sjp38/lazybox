a86b17b252531d02f20ba232cb0f624de2331c95
SeongJae Park <sj@kernel.org>
2026-01-31 14:25:52 -0800

    linux_hack/mm_tree_summary: show number of commits per branch
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- linux_hack/mm_tree_summary.py
+++ linux_hack/mm_tree_summary.py
@@ -3,9 +3,9 @@
 
 '''
 Show a summary of mm tree status.
+- Number of commits per branch for given sub-subsystems.
 
 TODO:
-- Number of commits per branch for given sub-subsystems.
 - Number of commits per branch for given sub-subsystems per review status
   - review stat
     - No review
@@ -23,20 +23,39 @@ os.sys.path.insert(0, os.path.abspath(
 
 import git_remote_name
 
-def nr_commits(linux_dir, commits_range):
+def commits_in(linux_dir, commits_range):
     git_cmd = ['git', '-C', linux_dir]
-    return len(subprocess.check_output(
-        git_cmd + ['log', '--pretty=%h', commits_range]
-        ).decode().strip().splitlines())
+    return subprocess.check_output(
+            git_cmd + ['log', '--pretty=%H', '--no-merges', commits_range]
+            ).decode().split()
 
 def pr_commits_per_mm_branches(linux_dir):
     mm_remote = git_remote_name.get_remote_name_for(
             linux_dir,
             'https://git.kernel.org/pub/scm/linux/kernel/git/akpm/mm.git')
-    nr_commits_mm_hotfixes_stable = nr_commits(
-            linux_dir, '%s/master..%s/mm-hotfixes-stable' %
-            (mm_remote, mm_remote))
-    print('mm-hotfixes-stable: %d commits' % nr_commits_mm_hotfixes_stable)
+
+    # it is unclear what branch is base of what branch.  Just give commit to
+    # unique branch, with the priorities.  Hotfixes are always important, and
+    # mm is more important than nonmm.
+    branches = ['mm-hotfixes-stable', 'mm-hotfixes-unstable',
+                'mm-stable', 'mm-unstable', 'mm-new',
+                'mm-nonmm-stable', 'mm-nonmm-unstable']
+    branch_commits = {}
+    categorized_commits = {}
+    for idx, branch in enumerate(branches):
+        commits = commits_in(
+                linux_dir, '%s/master..%s/%s' % (mm_remote, mm_remote, branch))
+        filtered_commits = []
+        for commit in commits:
+            if commit in categorized_commits:
+                continue
+            filtered_commits.append(commit)
+            categorized_commits[commit] = True
+        branch_commits[branch] = filtered_commits
+
+    for branch in branches:
+        print('%s: %d commits' % (branch, len(branch_commits[branch])))
+    print('Total: %d commits' % len(categorized_commits))
 
 def main():
     parser = argparse.ArgumentParser()
