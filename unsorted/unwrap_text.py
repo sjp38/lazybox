6debd9c56d3bd05215c3f226d2935e4ec37533c0
SeongJae Park <sj@kernel.org>
2026-06-19 12:30:11 -0700

    unsorted/unwrap_text: add --ignore_prefix_len option
    Sometimes we get the text to unwrap, from C code or git diff that may
    have some prefix that should be ignored first.  Support the case with a
    new option, --ignore_prefix_len.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- unsorted/unwrap_text.py
+++ unsorted/unwrap_text.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
+import argparse
 import sys
 
 def main():
-    lines = sys.stdin.read()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ignore_prefix_len', type=int,
+                        help='length of prefixes in input lines to ignore')
+    args = parser.parse_args()
+
+    lines = sys.stdin.read().split('\n')
+    if args.ignore_prefix_len > 0:
+        for idx, line in enumerate(lines):
+            lines[idx] = line[args.ignore_prefix_len:]
+
     collected_lines = []
-    for line in lines.split('\n'):
+    for line in lines:
         if line == '':
             print(' '.join(collected_lines))
             print()
