0338bdf57af073c31f5f5dc0d8ca93b56ce30695
SJ Park <sj@kernel.org>
2026-08-22 12:52:08 -0700

    vm: add a python script for starting a VM
    Signed-off-by: SJ Park <sj@kernel.org>

---
+++ vm/start_vm.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+import argparse
+import os
+import subprocess
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--disk', default='overlay.disk', metavar='<file>',
+                        help='disk to boot')
+    parser.add_argument('--nr_cores', type=int, metavar='<int>',
+                        help='number of cores for the VM')
+    parser.add_argument('--memory_mb', metavar='<mega_bytes>',
+                        help='memory size for the VM')
+    parser.add_argument('--ssh_port', default=2242,
+                        help='SSH port for the VM')
+    args = parser.parse_args()
+
+    if args.nr_cores is None:
+        res = subprocess.run(
+                ['nproc'], capture_output=True, text=True, check=True)
+        args.nr_cores = int(res.stdout) / 2
+
+    if args.memory_mb is None:
+        with open('/proc/meminfo', 'r') as f:
+            for line in f:
+                if not line.startswith('MemTotal:'):
+                    continue
+                fields = line.split()
+                total_mem_kb = int(fields[1])
+                args.memory_mb = int(total_mem_kb / 4 / 1024)
+                break
+    if args.memory_mb is None:
+        print('no Memtotal?')
+        exit(1)
+
+    cmd = [
+            'sudo', 'qemu-system-x86_64', '-enable-kvm', '-cpu', 'host',
+            '-smp', '%d' % args.nr_cores, '-m', '%s' % args.memory_mb,
+            '-drive', 'file=%s,if=virtio,cache=none' % args.disk,
+            '-net', 'user,hostfwd=tcp::%s-:22' % args.ssh_port, '-net', 'nic',
+            '-nographic']
+    os.execlp('sudo', *cmd)
+
+if __name__ == '__main__':
+    main()
