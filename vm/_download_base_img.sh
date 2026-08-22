d95734bdbb9916bbf9dc784dd86293c2e3312c3d
SJ Park <sj@kernel.org>
2026-08-22 12:51:33 -0700

    vm: add a script for downloading cloudimg base images
    Signed-off-by: SJ Park <sj@kernel.org>

---
+++ vm/_download_base_img.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+set -e
+
+if [ $# -ne 2 ]
+then
+	echo "Usage: $0 <debian13> <output path>"
+	exit 1
+fi
+
+target=$1
+output_path=$2
+
+case "$target" in
+	debian13)
+		link=https://cloud.debian.org/images/cloud/trixie/latest/debian-13-generic-amd64.qcow2
+		;;
+	*)
+		;;
+esac
+
+curl -L -o "$output_path" "$link"
